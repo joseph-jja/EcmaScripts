aae19e5495fb6ce9da8e2f861fb0b06962eac91b
Joseph Acosta <joseph.acosta@macys.com>
2016-06-28 14:20:33 -0700

    initial gulping

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
 var gulp = require('gulp'), 
+    jsbeautify = require("gulp-jsbeautifier");
     babel = require("gulp-babel");
 
 gulp.task('babel', () => {
 	return gulp.src("src/es6/**/**.js")
 		.pipe(babel({
-			presets: ['es2015']
+			
 		}))
 		.pipe(gulp.dest('dist'));
 });
 
 gulp.task('compile', () => {
-	return gulp.src("src/**/**.js")
-		.pipe(babel({
-			presets: ['es2015']
+	return gulp.src("src/js/**/**.js")
+		.pipe(jsbeautify({
+
 		}))
 		.pipe(gulp.dest('dist'));
 });
