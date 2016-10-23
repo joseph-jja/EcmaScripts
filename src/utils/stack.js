02bd35b67c9688b69d40c0746688ce59c02ce71f
Joe <josepha48@gmail.com>
2016-10-22 21:07:32 -0700

    update to make simple stack

--- src/utils/stack.js
+++ src/utils/stack.js
@@ -1,14 +1,12 @@
 // simple stack that a caller can call new on
+// this should just be a push pop and get type thing
 function stack() {
     this.index = 0;
     this.list = {};
 };
 
-stack.prototype.push = function (key, obj, cb) {
-    this.list[key] = {
-        data: obj,
-        callback: cb
-    };
+stack.prototype.push = function (key, obj) {
+    this.list[key] = obj;
     this.index++;
 };
 
@@ -23,16 +21,8 @@ stack.prototype.get = function (key) {
 
 stack.prototype.clear = function () {
     for (let o in this.list) {
-        if (o && this.list[o]) {
-            let obj = this.list[o];
-            try {
-                obj.callback && obj.callback(obj);
-                if (obj.data) {
-                    obj.data = undefined;
-                }
-            } catch (e) {
-                // do somehting with the exception?
-            }
+        if (o) {
+            this.list[o] = undefined;
         }
         this.index = 0;
     }
