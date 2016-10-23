ba37b5247b26c4b6d60816dcdd50e9d65aceed20
Joe <josepha48@gmail.com>
2016-10-22 21:24:57 -0700

    update web storage

--- src/client/utils/webStorage.js
+++ src/client/utils/webStorage.js
@@ -1,31 +1,59 @@
 // this allows us to check to see if session storage is actually enabled by the browser
-	// sometimes this will fail like when a browser does not have cookies enabled for a site
-var	storeEnabled = (function() {
-		var storeEnabled = false;
-		if( window.sessionStorage ) {
-			try { 
-				window.sessionStorage.setItem("testData", "dunny");
-				window.sessionStorage.removeItem("testData");
-				storeEnabled = true;
-			} catch(e) {
-				WebBrowser.log("sessionStorage exists but is not available.");
-			}
-		}
-		return storeEnabled;
-	})();
+// sometimes this will fail like when a browser does not have cookies enabled for a site
+var storeEnabled, localEnabled, sessionStore, localStore;
 
-	
-	// here we check to see if local storage is enabled or not
-var	localEnabled = (function() { 
-		var localEnabled = false;
-		try { 
-			if ( window.localStorage ) {
-				window.localStorage.setItem("testData", "dunny");
-				window.localStorage.removeItem("testData");
-				localEnabled = true;
-			}
-		} catch(e) {
-			WebBrowser.log("localStorage is not available.");
-		}
-		return localEnabled;
-	})();
+storeEnabled = (function () {
+    var sEnabled = false;
+    if (window.sessionStorage) {
+        try {
+            window.sessionStorage.setItem("testData", "dunny");
+            window.sessionStorage.removeItem("testData");
+            sEnabled = true;
+        } catch (e) {
+
+        }
+    }
+    return sEnabled;
+})();
+
+
+// here we check to see if local storage is enabled or not
+localEnabled = (function () {
+    var lEnabled = false;
+    try {
+        if (window.localStorage) {
+            window.localStorage.setItem("testData", "dunny");
+            window.localStorage.removeItem("testData");
+            lEnabled = true;
+        }
+    } catch (e) {
+
+    }
+    return localEnabled;
+})();
+
+import * as store from "../../utils/store";
+
+if (storeEnabled) {
+    sessionStore = window.sessionStorage;
+} else {
+    sessionStore = new store();
+}
+
+if (localEnabled) {
+    localStore = window.localStorage;
+} else {
+    localStore = new store();
+}
+
+// used for testing mainly but can be a simple key - value store
+export function createKeyStore() {
+    return new store();
+};
+
+export {
+    sessionStore,
+    sessionEnabled,
+    localStore,
+    localEnabled
+};
