26e5fabab9a876935a047ca5732db7ade2306cd3
Joe <josepha48@gmail.com>
2016-10-22 20:33:01 -0700

    beautify and fix

--- src/utils/stack.js
+++ src/utils/stack.js
@@ -1,30 +1,39 @@
 // simple stack that a caller can call new on
-var stack = function() {
+function stack() {
     this.index = 0;
     this.list = {};
 };
 
-stack.push = function(key, obj, cb) {
-        this.list[key] = { data: obj, callback: cb };
-        this.index++;
+stack.prototype.push = function (key, obj, cb) {
+    this.list[key] = {
+        data: obj,
+        callback: cb
+    };
+    this.index++;
 };
 
-stack.pop = function(key) {
-        this.list[key] = null;
-        this.index--;
+stack.prototype.pop = function (key) {
+    this.list[key] = null;
+    this.index--;
 };
 
-stack.clear = function() {
-    for ( let o this.list ) {
-       if ( o && this.list[o] ) {
-          let obj = this.list[o];
-          try { 
-            obj.callback && obj.callback( obj );
-            if ( obj.data ) { obj.data = undefined; } 
-          } catch (e) { 
-              // do somehting with the exception?
-          }  
-       }
-       this.index = 0;
+stack.prototype.clear = function () {
+    for (let o in this.list) {
+        if (o && this.list[o]) {
+            let obj = this.list[o];
+            try {
+                obj.callback && obj.callback(obj);
+                if (obj.data) {
+                    obj.data = undefined;
+                }
+            } catch (e) {
+                // do somehting with the exception?
+            }
+        }
+        this.index = 0;
     }
-export stack;
+};
+
+export {
+    stack
+};
