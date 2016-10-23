7e01b37c3b31f638a87e5a3f0ea864091147eb90
Joe <josepha48@gmail.com>
2016-10-22 20:38:41 -0700

    fix and beautify

--- src/dom/selector.js
+++ src/dom/selector.js
@@ -4,32 +4,30 @@
 // this is done in a UMD format
 var rquickExpr = /^#(?:([\w-]+)|(\w+)|\.([\w-]+))$/;
 
-export selector function(expr, parent) {
+export function selector(expr, parent) {
 
-        var result, len;
+    var result, len;
 
-        if (arguments.length < 2 || typeof parent === 'undefined') {
-            parent = d;
-        }
+    if (arguments.length < 2 || typeof parent === 'undefined') {
+        parent = d;
+    }
 
-        if (rquickExpr.test(expr)) {
-            // remove the leading # and return array of 1 or 0
-            result = parent.getElementById(expr.substring(1));
-            result = (result ? [result] : []);
-        } else {
-            result = parent.querySelectorAll(expr);
-        }
+    if (rquickExpr.test(expr)) {
+        // remove the leading # and return array of 1 or 0
+        result = parent.getElementById(expr.substring(1));
+        result = (result ? [result] : []);
+    } else {
+        result = parent.querySelectorAll(expr);
+    }
 
-        this.get = function(i) {
-            return result[i];
-        };
-
-        if (result && result.length) {
-            this.length = result.length;
-        } else if (result) {
-            this.length = 1;
-        }
-        return this;
+    this.get = function (i) {
+        return result[i];
     };
 
-    
+    if (result && result.length) {
+        this.length = result.length;
+    } else if (result) {
+        this.length = 1;
+    }
+    return this;
+};
