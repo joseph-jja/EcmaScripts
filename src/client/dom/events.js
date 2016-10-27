260f44ed03d59d62e7c62213f3db07e48391e923
Joe Acosta <josepha48@gmail.com>
2016-10-27 08:48:09 -0700

    beautify events and add build script

--- src/client/dom/events.js
+++ src/client/dom/events.js
@@ -1,111 +1,110 @@
-var addEvent, 
-removeEvent, 
-getEventPosX, 
-getEventPosY, 
-getTarget
-getEvent, 
-createEvent, 
-fireEvent;
+var addEvent,
+    removeEvent,
+    getEventPosX,
+    getEventPosY,
+    getTarget,
+    getEvent,
+    createEvent,
+    fireEvent;
 
 if ( window.attachEvent && !window.addEventListener ) {
-        addEvent = function ( obj, eventType, fn, capture ) {
-            var result;
-            result = obj.attachEvent( "on" + eventType, fn );
-            if ( !result ) {
-                throw ( "Event " + eventType + " could not be added!" );
-            }
-        };
-    } else if ( window.addEventListener ) {
-        addEvent = function ( obj, eventType, fn, capture ) {
-            var cap = ( capture ) ? capture : false;
-            obj.addEventListener( eventType, fn, cap );
-        };
-    }
-
-    if ( window.detachEvent && !window.removeEventListener ) {
-       removeEvent = function ( obj, eventType, fn, capture ) {
-            var result = obj.detachEvent( "on" + eventType, fn );
-            if ( !result ) {
-                throw ( "Event " + eventType + " could not be removed!" );
-            }
-        };
-    } else if ( window.removeEventListener ) {
-        removeEvent = function ( obj, eventType, fn, capture ) {
-            obj.removeEventListener( eventType, fn, capture );
-        };
-    }
-
-    getEvent = function ( evt ) {
-        var eventObj = ( window.event ) ? window.event : evt;
-        return eventObj;
-    };
-
-    getTarget = function (evt) {
-         var eventObj = getEvent(eventObj), 
-         result;
-        if ( eventObj.srcElement ) {
-            result = eventObj.srcElement;
-        } else if ( eventObj.target ) {
-            result = eventObj.target;
+    addEvent = function ( obj, eventType, fn, capture ) {
+        var result;
+        result = obj.attachEvent( "on" + eventType, fn );
+        if ( !result ) {
+            throw ( "Event " + eventType + " could not be added!" );
         }
-        return result;
     };
+} else if ( window.addEventListener ) {
+    addEvent = function ( obj, eventType, fn, capture ) {
+        var cap = ( capture ) ? capture : false;
+        obj.addEventListener( eventType, fn, cap );
+    };
+}
 
-    getEventPosX = function (evt) {
-         var eventObj = getEvent(eventObj), 
-         result = 0;
-        if ( eventObj.pageX ) {
-            result = eventObj.pageX;
-        } else if ( eventObj.clientX ) {
-            result = eventObj.clientX;
+if ( window.detachEvent && !window.removeEventListener ) {
+    removeEvent = function ( obj, eventType, fn, capture ) {
+        var result = obj.detachEvent( "on" + eventType, fn );
+        if ( !result ) {
+            throw ( "Event " + eventType + " could not be removed!" );
         }
-        return result;
     };
-
-    getEventPosY = function (evt) {
-         var eventObj = getEvent(eventObj), 
-         result = 0;
-        if ( eventObj.pageY ) {
-            result =  eventObj.pageY;
-        } else if ( eventObj.clientY ) {
-            result =  eventObj.clientY;
-        }
-        return result;
+} else if ( window.removeEventListener ) {
+    removeEvent = function ( obj, eventType, fn, capture ) {
+        obj.removeEventListener( eventType, fn, capture );
     };
+}
 
-    if ( document.createEvent ) {
-        createEvent = function ( name, obj, options ) {
-            var evt;
-            evt = document.createEvent( "Event" );
-            if ( !event ) {
-                return undefined;
-            }
-            if ( !options ) {
-                options = {};
-            }
-            if ( !options.canBubble ) {
-                options.canBubble = true;
-            }
-            if ( !options.cancellable ) {
-                options.cancellable = true;
-            }
-            evt[ "initEvent" ]( "Event", options.canBubble, options.cancellable );
-            return evt;
-        };
+getEvent = function ( evt ) {
+    var eventObj = ( window.event ) ? window.event : evt;
+    return eventObj;
+};
 
-        fireEvent = function ( evt ) {
-            document.dispatchEvent( evt );
-        };
+getTarget = function ( evt ) {
+    var eventObj = getEvent( eventObj ),
+        result;
+    if ( eventObj.srcElement ) {
+        result = eventObj.srcElement;
+    } else if ( eventObj.target ) {
+        result = eventObj.target;
     }
+    return result;
+};
 
-export { addEvent, 
-removeEvent, 
-getEventPosX, 
-getEventPosY, 
-getTarget
-getEvent, 
-createEvent, 
-fireEvent};
+getEventPosX = function ( evt ) {
+    var eventObj = getEvent( eventObj ),
+        result = 0;
+    if ( eventObj.pageX ) {
+        result = eventObj.pageX;
+    } else if ( eventObj.clientX ) {
+        result = eventObj.clientX;
+    }
+    return result;
+};
 
+getEventPosY = function ( evt ) {
+    var eventObj = getEvent( eventObj ),
+        result = 0;
+    if ( eventObj.pageY ) {
+        result = eventObj.pageY;
+    } else if ( eventObj.clientY ) {
+        result = eventObj.clientY;
+    }
+    return result;
+};
 
+if ( document.createEvent ) {
+    createEvent = function ( name, obj, options ) {
+        var evt;
+        evt = document.createEvent( "Event" );
+        if ( !event ) {
+            return undefined;
+        }
+        if ( !options ) {
+            options = {};
+        }
+        if ( !options.canBubble ) {
+            options.canBubble = true;
+        }
+        if ( !options.cancellable ) {
+            options.cancellable = true;
+        }
+        evt[ "initEvent" ]( "Event", options.canBubble, options.cancellable );
+        return evt;
+    };
 
+    fireEvent = function ( evt ) {
+        document.dispatchEvent( evt );
+    };
+}
+
+export {
+    addEvent,
+    removeEvent,
+    getEventPosX,
+    getEventPosY,
+    getTarget,
+    getEvent,
+    createEvent,
+    fireEvent
+};
