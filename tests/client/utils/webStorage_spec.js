040b63a95de7d168f66835b1cae64e34e0ef327b
Joseph Acosta <joseph.acosta@macys.com>
2016-10-31 15:52:37 -0700

    add more tests

--- tests/client/utils/webStorage_spec.js
+++ tests/client/utils/webStorage_spec.js
@@ -9,4 +9,18 @@ describe( "tests on web storage object", function () {
     it( "is localEnabled test", function () {
         expect( ws.localEnabled ).toEqual( true );
     } );
+
+    it( "set and remove item from session test", function () {
+        ws.sessionStore.setItem( "testData", "dunny" );
+        expect( ws.sessionStore.getItem( "testData" ) ).toEqual( "dunny" );
+        ws.sessionStore.removeItem( "testData" );
+        expect( ws.sessionStore.getItem( "testData" ) ).toBe( null );
+    } );
+
+    it( "set and remove item from local test", function () {
+        ws.localStore.setItem( "testData", "dunny" );
+        expect( ws.localStore.getItem( "testData" ) ).toEqual( "dunny" );
+        ws.localStore.removeItem( "testData" );
+        expect( ws.localStore.getItem( "testData" ) ).toBe( null );
+    } );
 } );
