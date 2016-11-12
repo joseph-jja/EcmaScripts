e9cb75c1791de88943aa004c80a6c728042c6942
Joe <josepha48@gmail.com>
2016-11-11 20:54:04 -0800

    add tests for cookie

--- tests/utils/cookie_spec.js
+++ tests/utils/cookie_spec.js
@@ -1,1 +1,19 @@
 
+import * as cookie from "utils/cookie";
+
+describe( "cookie functions tests", function () {
+
+    it( "test create functions", function () {
+      var val;
+     
+      cookie.set( 'hello', 'world' );
+      val = cookie.get( 'hello' );
+      expect( val ).toEqual( 'world' );
+    
+      cookie.remove( 'hello' );
+      val = cookie.get( 'hello' );
+      expect( val ).toBe( undefined );
+    
+    } );
+  
+} );
