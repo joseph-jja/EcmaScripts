dac98d7a2489a121ae151934626c84f942cdec38
Joe <josepha48@gmail.com>
2018-04-29 21:17:51 -0700

    es6 - ify

--- src/utils/mathFunctions.js
+++ src/utils/mathFunctions.js
@@ -23,14 +23,14 @@
  * extend the Math object in JavaScript
  * the Math object is static, so you do not call new on it, just set a reference to it
  */
-var MathFunctions = {};
+let MathFunctions = {};
 
 /**
  * add(x,y, ...)                Returns x + y [can handle more than 2 parameters ]
  */
 MathFunctions.add = function () {
-    var result = 0;
-    for ( var x = 0; x < arguments.length; x += 1 ) {
+    let result = 0;
+    for ( let x = 0; x < arguments.length; x += 1 ) {
         result += ( +arguments[ x ] );
     }
     return result;
@@ -47,9 +47,9 @@ MathFunctions.subtract = function ( x, y ) {
  * multiply(x,y, ...)         Returns x * y   [can handle more than 2 parameters ]
  */
 MathFunctions.multiply = function () {
-    var result = 1;
-    for ( var x = 0; x < arguments.length; x += 1 ) {
-        var y = result;
+    let result = 1;
+    for ( let x = 0; x < arguments.length; x += 1 ) {
+        let y = result;
         result = ( +y ) * ( +arguments[ x ] );
     }
     return result;
@@ -83,9 +83,9 @@ MathFunctions.cube = function ( x ) {
  * using the pythagorean theorem ( x^2 + y^2 = z^2) and solving for z
  */
 MathFunctions.computeLineLength = function ( x1, y1, x2, y2 ) {
-    var xdiff = MathFunctions.subtract( x2, x1 );
-    var ydiff = MathFunctions.subtract( y2, y1 );
-    var total = MathFunctions.add( MathFunctions.square( xdiff ), MathFunctions.square( ydiff ) );
+    let xdiff = MathFunctions.subtract( x2, x1 );
+    let ydiff = MathFunctions.subtract( y2, y1 );
+    let total = MathFunctions.add( MathFunctions.square( xdiff ), MathFunctions.square( ydiff ) );
     return Math.sqrt( total );
 };
 
@@ -93,10 +93,10 @@ MathFunctions.computeLineLength = function ( x1, y1, x2, y2 ) {
  * computes the perimeter of a 4 sided object, given 4 points
  */
 MathFunctions.computePerimeter = function ( x1, y1, x2, y2, x3, y3, x4, y4 ) {
-    var line1 = MathFunctions.computeLineLength( x1, y1, x2, y2 );
-    var line2 = MathFunctions.computeLineLength( x2, y2, x3, y3 );
-    var line3 = MathFunctions.computeLineLength( x3, y3, x4, y4 );
-    var line4 = MathFunctions.computeLineLength( x4, y4, x1, y1 );
+    let line1 = MathFunctions.computeLineLength( x1, y1, x2, y2 );
+    let line2 = MathFunctions.computeLineLength( x2, y2, x3, y3 );
+    let line3 = MathFunctions.computeLineLength( x3, y3, x4, y4 );
+    let line4 = MathFunctions.computeLineLength( x4, y4, x1, y1 );
     return MathFunctions.computePerimeterByLength( line1, line2, line3, line4 );
 };
 
@@ -121,7 +121,7 @@ MathFunctions.areaOfTriangle = function ( base, height ) {
 
 /* compute area of a trapizoid */
 MathFunctions.areaOfTrapizoid = function ( height, abase, bbase ) {
-    var a1, a2;
+    let a1, a2;
     a1 = MathFunctions.add( abase, bbase );
     a2 = MathFunctions.divide( a1, 2 );
     return MathFunctions.multiply( height, a2 );
@@ -133,7 +133,7 @@ MathFunctions.pythagorean = function ( size, psize ) {
 
 /* compute average */
 MathFunctions.average = function () {
-    var sum = 0;
+    let sum = 0;
     Object.keys( arguments ).forEach( ( e ) => {
         sum = MathFunctions.add( sum, arguments[ e ] );
     } );
@@ -152,9 +152,9 @@ MathFunctions.circumferenceOfACircle = function ( radius ) {
 
 /* compute the surface area of a cone */
 MathFunctions.surfaceAreaCone = function ( radius, height ) {
-    var sqrtTotal = Math.sqrt( MathFunctions.multiply( Math.PI, MathFunctions.square( radius ) ) );
-    var sumtotal = MathFunctions.add( MathFunctions.square( radius ), MathFunctions.square( height ) );
-    var radiuisTotal = MathFunctions.multiply( radius, Math.PI, sumtotal );
+    let sqrtTotal = Math.sqrt( MathFunctions.multiply( Math.PI, MathFunctions.square( radius ) ) );
+    let sumtotal = MathFunctions.add( MathFunctions.square( radius ), MathFunctions.square( height ) );
+    let radiuisTotal = MathFunctions.multiply( radius, Math.PI, sumtotal );
     return MathFunctions.add( sqrtTotal, radiuisTotal );
 };
 
@@ -165,8 +165,8 @@ MathFunctions.volumeCone = function ( radius, height ) {
 
 /* compute the surface area of a cylinder */
 MathFunctions.surfaceAreaCylinder = function ( radius, height ) {
-    var radiusTotal = MathFunctions.multiply( 2, Math.PI, MathFunctions.square( radius ) );
-    var heightTotal = MathFunctions.multiply( 2, Math.PI, radius, height );
+    let radiusTotal = MathFunctions.multiply( 2, Math.PI, MathFunctions.square( radius ) );
+    let heightTotal = MathFunctions.multiply( 2, Math.PI, radius, height );
     return MathFunctions.add( radiusTotal, heightTotal );
 };
 
@@ -199,7 +199,7 @@ MathFunctions.factorial = function ( x ) {
     if ( ( +x ) <= 1 ) {
         return 1;
     }
-    var f = ( +x ) - 1;
+    let f = ( +x ) - 1;
     return MathFunctions.multiply( x, MathFunctions.factorial( f ) );
 };
 
@@ -209,10 +209,10 @@ MathFunctions.hexidecimal = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", 
  * not so good at converting from base 10 to base 10
  * up to base 16 or HEX */
 MathFunctions.convertFromBaseTenToBaseX = function ( xbase, inval ) {
-    var remainder = MathFunctions.hexidecimal[ inval % xbase ];
+    let remainder = MathFunctions.hexidecimal[ inval % xbase ];
     while ( inval >= xbase ) {
-        var r1 = MathFunctions.subtract( inval, ( inval % xbase ) );
-        var inval = MathFunctions.divide( r1, xbase );
+        let r1 = MathFunctions.subtract( inval, ( inval % xbase ) );
+        let inval = MathFunctions.divide( r1, xbase );
         // in this case we do not want to add we want to append the strings together
         if ( inval >= xbase ) {
             remainder = MathFunctions.hexidecimal[ inval % xbase ] + remainder;
@@ -226,11 +226,11 @@ MathFunctions.convertFromBaseTenToBaseX = function ( xbase, inval ) {
 /* convert from base x to base ten
  * up to base 16 or HEX */
 MathFunctions.convertFromBaseXToBaseTen = function ( xbase, inval ) {
-    var remainder = 0;
-    var num = 1;
+    let remainder = 0;
+    let num = 1;
 
     function valueToInt( x ) {
-        for ( var p = 0; p < MathFunctions.hexidecimal.length; p += 1 ) {
+        for ( let p = 0; p < MathFunctions.hexidecimal.length; p += 1 ) {
             if ( MathFunctions.hexidecimal[ p ] === x ) {
                 return p;
             }
@@ -238,9 +238,9 @@ MathFunctions.convertFromBaseXToBaseTen = function ( xbase, inval ) {
         return '';
     }
     while ( num <= inval.length ) {
-        var incount = Math.pow( xbase, MathFunctions.subtract( num, 1 ) );
-        var ivlen = MathFunctions.subtract( inval.length, num );
-        var sschar = valueToInt( inval.charAt( ivlen ) );
+        let incount = Math.pow( xbase, MathFunctions.subtract( num, 1 ) );
+        let ivlen = MathFunctions.subtract( inval.length, num );
+        let sschar = valueToInt( inval.charAt( ivlen ) );
         remainder = MathFunctions.add( remainder, MathFunctions.multiply( sschar, incount ) );
         num++;
     }
@@ -256,7 +256,7 @@ MathFunctions.radiansToDegrees = function ( x ) {
 };
 
 MathFunctions.generateGUID = function () {
-    var timenow, hexval, guid, l, i, result;
+    let timenow, hexval, guid, l, i, result;
     timenow = new Date().getTime() * 1000000;
     hexval = MathFunctions.convertFromBaseTenToBaseX( 16, timenow );
     l = hexval.length;
