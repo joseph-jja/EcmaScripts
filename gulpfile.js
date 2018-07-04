a61bf1e78c7a1b5951233d59ac9fa0693c70cce8
Joe Acosta <josepha48@gmail.com>
2018-07-04 00:29:04 -0700

    update build, gulp beautifies only and webpack does the rest

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,11 @@ const gulp = require( 'gulp' ),
     jsbeautify = require( "gulp-jsbeautifier" ),
     fs = require( "fs" ),
     path = require( 'path' ),
-    webpack = require( 'gulp-webpack' ),
-    wpConfig = require( "./config/webpack" ),
     eslint = require( "gulp-eslint" );
 
 const baseDir = process.cwd(),
     jsConfig = JSON.parse( fs.readFileSync( `${baseDir}/config/js-beautify.json` ).toString() ),
     eslintCfg = `${baseDir}/config/eslint.cfg`;
-
-const webpackConfig = Object.assign( {}, wpConfig, {
-    "eslint": {
-        "configFile": path.resolve( "./config/eslint.cfg" )
-    }
-} );
 
 gulp.task( 'default', () => {
     gulp.src( "gulpfile.js" )
@@ -36,8 +28,5 @@ gulp.task( 'default', () => {
 
     return gulp.src( "src/**/**.js" )
         .pipe( jsbeautify( jsConfig ) )
-        .pipe( eslint( eslintCfg ) )
-        .pipe( gulp.dest( 'src' ) )
-        .pipe( webpack( webpackConfig ) )
-        .pipe( gulp.dest( '.' ) );
+        .pipe( gulp.dest( 'src' ) );
 } );
