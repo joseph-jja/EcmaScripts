0c2f43765259a8405aa563fe8d4ac494c973b7c7
Joe <josepha48@gmail.com>
2018-07-08 17:35:11 -0700

    small update

--- src/client/net/fetcher.js
+++ src/client/net/fetcher.js
@@ -5,19 +5,23 @@ const BufferDecoder = new TextDecoder("ascii", { ignoreBOM: false });
 
 // FIXME or FINISH-ME :P
 function fetcher( url ) {
-    return fetch(url).then ( (response) => {
+    return fetch(url).then ( response => {
         const reader = response.body.getReader();
-        let results = '';
-        reader.read().then(({ done, value }) => {
+
+        const dataConverter = ({ done, value }) => {
             if ( value ) {
                 const data = BufferDecoder.decode(new Int8Array(value));
                 results += data;
+                return reader.read().then(dataConverter);
             }
             // Is there no more data to read?
             if (done) {
               // Tell the browser that we have finished sending data
-              return Promise.resolve(results);
+              // resolve this and return data ? as we have results :/
+              return;
             }
-        });
+        };
+        
+        return reader.read().then(dataConverter).then( r => { console.log(r); });
     });
 }
