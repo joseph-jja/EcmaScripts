51d9613ecf3b87ec442b29243b21f7754d164b60
Joe Acosta <josepha48@gmail.com>
2018-08-10 13:46:15 -0700

    add a test for the footer

---
+++ tests/client/components/footer_spec.js
@@ -0,0 +1,15 @@
+import setFooter from 'client/components/footer';
+
+describe( 'footer tests', () => {
+
+    const nav = document.createElement( 'div' );
+    nav.id = 'footer-space';
+
+    document.body.appendChild( nav );
+
+    it( 'setFooter  test', () => {
+        setFooter( nav );
+        const footerLis = document.querySelectorAll( '#footer-space ul li' );
+        expect( footerLis.length ).toEqual( 3 );
+    } );
+} );
