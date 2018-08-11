3e6f73d0ca5502699b727ea7037c5635764f63b2
Joe <josepha48@gmail.com>
2018-08-10 19:52:35 -0700

    simple test

---
+++ tests/client/net/xmlhttp_spec.js
@@ -0,0 +1,9 @@
+import XmlHttp from "client/net/xmlhttp";
+
+describe( 'testing xmlhttp', () => {
+
+    it( 'BinaryClock  test', () => {
+         const request = new XmlHttp();
+         expect(request.open).toBeDefined();
+    } );
+} );
