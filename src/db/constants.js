87e3ebc0ada52c4ce2125739237de04c72f2e769
Joe Acosta <josepha48@gmail.com>
2018-12-14 13:16:31 -0800

    update constants file

--- src/db/constants.js
+++ src/db/constants.js
@@ -1,4 +1,12 @@
-export const DBName = 'tasksDB',
+const DBName = 'tasksDB',
     StoreName = 'tasks',
     DB_SUCCESS = 200,
     DB_ERROR = 500;
+
+export { 
+    DBName,
+    StoreName,
+    DB_SUCCESS,
+    DB_ERROR
+};
+
