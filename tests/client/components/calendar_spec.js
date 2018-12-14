3178b05e7c5c600d063447b3abe14b4f9b35ded4
Joe Acosta <josepha48@gmail.com>
2018-12-14 14:10:43 -0800

    updates for coverage

--- tests/client/components/calendar_spec.js
+++ tests/client/components/calendar_spec.js
@@ -2,8 +2,32 @@ import Calendar from 'client/components/calendar';
 
 describe( 'testing calendar', () => {
 
+    let parent;
+
+    beforeEach( () => {
+        if ( !parent ) {
+            parent = document.createElement( 'div' );
+        }
+        parent.id = 'container-for-calendar';
+        document.body.appendChild( parent );
+    } );
+
+    afterEach( () => {
+        if ( parent ) {
+            document.body.removeChild( parent );
+        }
+    } );
+
     it( 'calendar test', () => {
-        const mycal = new Calendar(document.body);
+        const mycal = new Calendar( parent.id );
         expect( mycal ).toBeDefined();
+    } );
+
+    it( 'calendar date test', () => {
+        const mycal = new Calendar( parent.id );
+        mycal.render();
+        mycal.setCalendarDate( 5, 12, 2012 );
+        const mdate = mycal.getCalendarDate();
+        expect( mdate ).toEqual( 'June 12, 2012' );
     } );
 } );
