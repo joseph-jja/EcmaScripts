8ba7c429771e8535f3c22a8e2d631b430894ffc6
Joe Acosta <josepha48@gmail.com>
2018-12-21 14:31:50 -0800

    add more tests

--- tests/client/components/wbWindow_spec.js
+++ tests/client/components/wbWindow_spec.js
@@ -2,7 +2,42 @@ import wbWindow from 'client/components/wbWindow';
 
 describe( 'testing wbWindow', () => {
 
+  let parent;
+
+  beforeEach( () => {
+      if ( !parent ) {
+          parent = document.createElement( 'div' );
+      }
+      parent.id = 'container-for-webwindow';
+      document.body.appendChild( parent );
+  } );
+
+  afterEach( () => {
+      if ( parent ) {
+          document.body.removeChild( parent );
+      }
+  } );
+
     it( 'wbWindow test', () => {
         expect( wbWindow ).toBeDefined();
+    } );
+
+    it( 'wbWindow create test', () => {
+      const win = new wbWindow( parent.id );
+      expect( win ).toBeDefined();
+    } );
+
+    it( 'wbWindow create test', () => {
+      const win = new wbWindow( parent.id );
+      spyOn(win, 'setTitle');
+      win.setTitle('This is a test');
+      expect( win.setTitle ).toHaveBeenCalled();
+    } );
+
+    it( 'wbWindow create test', () => {
+      const win = new wbWindow( parent.id );
+      spyOn(win, 'disableDrag');
+      win.disableDrag();
+      expect( win.disableDrag ).toHaveBeenCalled();
     } );
 } );
