43ace550fb8206a2f3b72491207a7b84966ba054
Joe <josepha48@gmail.com>
2019-01-19 21:17:56 -0800

    switch to chrome headless by default

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -105,8 +105,8 @@ module.exports = function ( config ) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        //browsers: [ 'ChromeHeadless' ],
-        browsers: [ 'jsdom' ],
+        browsers: [ 'ChromeHeadless' ],
+        //browsers: [ 'jsdom' ],
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
