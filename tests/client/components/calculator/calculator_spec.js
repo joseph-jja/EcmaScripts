7ee986265f962485d3329b35cab685de1027216f
Joe <josepha48@gmail.com>
2019-01-21 16:08:05 -0800

    add another test for the calculator

--- tests/client/components/calculator/calculator_spec.js
+++ tests/client/components/calculator/calculator_spec.js
@@ -1,8 +1,31 @@
-import * as Calculator from 'client/components/calculator/Calculator';
+import Calculator from 'client/components/calculator/Calculator';
 
 describe( 'testing calculator', () => {
 
+    let parent;
+
+    beforeEach( () => {
+        if ( !parent ) {
+            parent = document.createElement( 'div' );
+        }
+        parent.id = 'container-for-calculator';
+        document.body.appendChild( parent );
+    } );
+
+    afterEach( () => {
+        if ( parent ) {
+            document.body.removeChild( parent );
+        }
+    } );
+
     it( 'calculator test', () => {
         expect( Calculator ).toBeDefined();
+    } );
+
+    it( 'calculator render test', () => {
+        const calc = new Calculator( parent.id );
+        spyOn( calc, 'render' );
+        calc.render();
+        expect( calc.render ).toHaveBeenCalled();
     } );
 } );
