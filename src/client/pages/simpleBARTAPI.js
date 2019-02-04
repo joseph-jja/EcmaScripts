5efa5094807df9e1a7eb8126b52c27ce94089d82
Joe <josepha48@gmail.com>
2019-02-03 21:00:29 -0800

    shell updates

--- src/client/pages/simpleBARTAPI.js
+++ src/client/pages/simpleBARTAPI.js
@@ -1,1 +1,16 @@
+import selector from 'client/dom/selector';
+import * as events from 'client/dom/events';
+import * as dom from 'client/dom/DOM';
+
+// components
 import StationList from 'client/components/bart/stationsUI';
+import footer from 'client/components/footer';
+import * as menu from 'client/components/menu';
+
+function doOnLoadStuff() {
+  
+    
+  
+}
+
+events.addOnLoad( doOnLoadStuff );
