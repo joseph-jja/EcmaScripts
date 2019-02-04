1df57861ede127f38da5915af43668b7adb66dd6
Joe <josepha48@gmail.com>
2019-02-03 20:38:57 -0800

    create station list

--- src/client/components/bart/stationsUI.js
+++ src/client/components/bart/stationsUI.js
@@ -1,1 +1,36 @@
+import * as API from 'client/components/bart/api';
+import * as Storage from 'client/browser/webStorage';
 
+const STORAGE_KEY = 'SimpleBARTAPIStationList";
+
+export default async function StationList() {
+  
+  let stations = [];
+  
+  if ( Storage.localEnabled ) {
+       stations = Storage.localStore.getItem('STORAGE_KEY');
+  }
+  
+  if ( !stations ) {
+       stations = await API.getStations();
+  }
+  
+  if ( !stations) { 
+      throw( 'Could not get statsions!' );
+  }
+
+  if ( Storage.localEnabled ) {
+       stations = Storage.localStore.setItem(STORAGE_KEY, stations);
+  }
+
+  const items = stations.map( station => {
+    
+    return `<div data-abbr="${station.abbr}">${station.name}</div>`; 
+    
+  }).reduce( (acc, item) => {
+     return acc + item;
+  });
+  
+  return `<div id="bart-stations-list">${station.name}</div>`; 
+  
+}
