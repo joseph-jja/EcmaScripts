cb6e0c61d3e5c7cb3e2c30fc0a30436e5ceab029
Joe <josepha48@gmail.com>
2019-02-03 21:20:45 -0800

    basic shell

--- src/client/pages/simpleBARTAPI.js
+++ src/client/pages/simpleBARTAPI.js
@@ -4,13 +4,18 @@ import * as dom from 'client/dom/DOM';
 
 // components
 import StationList from 'client/components/bart/stationsUI';
+import TrainList from 'client/components/bart/trainsUI';
+import Alerts from 'client/components/bart/alertUI';
 import footer from 'client/components/footer';
 import * as menu from 'client/components/menu';
 
-function doOnLoadStuff() {
+async function doOnLoadStuff() {
   
+      const stationData = await StationList();
     
-  
+  // get dom node to put this data in and inner html it
+  // the add event listener
+    
 }
 
 events.addOnLoad( doOnLoadStuff );
