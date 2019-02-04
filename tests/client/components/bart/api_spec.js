57ebc00f4c6d98549e703db9e4ebf4d5b79fe2f2
Joe <josepha48@gmail.com>
2019-02-03 21:40:04 -0800

    remove spy

--- tests/client/components/bart/api_spec.js
+++ tests/client/components/bart/api_spec.js
@@ -2,14 +2,6 @@ import * as API from 'client/components/bart/api';
 
 describe( 'bart api tests', () => {
 
-    beforeAll( () => {
-        spyOn( fetch ).and.callFake( ( url ) => {
-
-            // switch on url and return different json data
-
-        } );
-    } );
-
     it( 'api should exist', () => {
         expect( API ).not.toBeUndefined();
     } );
