188b0ea9918889dee51d13b8327a7b0688237258
Joe Acosta <joe.acosta@sephora.com>
2019-02-11 11:25:16 -0800

    more tests

--- tests/client/dom/events_spec.js
+++ tests/client/dom/events_spec.js
@@ -2,10 +2,72 @@ import * as events from "client/dom/events";
 
 describe( "tests on events object", function () {
 
+    let parent;
+
+    beforeEach( () => {
+        if ( !parent ) {
+            parent = document.createElement( 'div' );
+        }
+        parent.id = 'container-for-events-tests';
+        document.body.appendChild( parent );
+    } );
+
+    afterEach( () => {
+        if ( parent ) {
+            document.body.removeChild( parent );
+        }
+    } );
     it( "tests for is touch", function () {
 
         const gotTouch = events.isTouchEnabled();
 
         expect( gotTouch ).not.toBeUndefined();
+    } );
+
+    it( "tests for get event", function () {
+
+        events.addEvent( parent, 'click', ( e ) => {
+
+            const evt = events.getEvent( e );
+            expect( evt ).not.toBeUndefined();
+
+        } );
+        parent.click();
+    } );
+
+    it( "tests for get event target", function () {
+
+        events.addEvent( parent, 'click', ( e ) => {
+
+            const evt = events.getEvent( e );
+            const tgt = events.getTarget( evt );
+            expect( tgt ).not.toBeUndefined();
+
+        } );
+        parent.click();
+    } );
+
+    it( "tests for get event x", function () {
+
+        events.addEvent( parent, 'click', ( e ) => {
+
+            const evt = events.getEvent( e );
+            const tgt = events.getEventPosX( evt );
+            expect( tgt ).toEqual(jasmine.any(Number));
+
+        } );
+        parent.click();
+    } );
+
+    it( "tests for get event y", function () {
+
+        events.addEvent( parent, 'click', ( e ) => {
+
+            const evt = events.getEvent( e );
+            const tgt = events.getEventPosY( evt );
+            expect( tgt ).toEqual(jasmine.any(Number));
+
+        } );
+        parent.click();
     } );
 } );
