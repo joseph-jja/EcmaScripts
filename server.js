317f34bdc36053bd50b36eab9e1e6b027f9eb296
Joe Acosta <joe.acosta@sephora.com>
2019-02-11 16:50:17 -0800

    support html and js content types

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const http = require( 'http' ),
     baseDir = process.cwd();
 
 const statfile = util.promisify( fs.stat ),
-    readfile = util.promisify( fs.readFile );
-readdir = util.promisify( fs.readdir );
+    readfile = util.promisify( fs.readFile ),
+    readdir = util.promisify( fs.readdir );
 
 const intialFile = `${baseDir}/coverage/report-html/index.html`;
 
-async function listDir( dir, response ) {
+async function listDir( dir, response, type ) {
     const isDir = await statfile( dir );
     let results;
     if ( isDir.isDirectory() ) {
@@ -24,9 +24,13 @@ async function listDir( dir, response ) {
 
     }
     response.writeHead( 200, {
-        'Content-Typei': 'text/html'
+        'Content-Typei': type
     } );
-    response.end( `<html><body>${results}</body></html>` );
+    if ( type === 'text/html' ) {
+        response.end( `<html><body>${results}</body></html>` );
+    } else {
+        response.end( results );
+    }
 }
 
 const server = http.createServer( ( request, response ) => {
@@ -35,7 +39,8 @@ const server = http.createServer( ( request, response ) => {
     if ( request.url !== '/' && !request.url.startsWith( '/js' ) ) {
         start = `${baseDir}/coverage/report-html${request.url}`;
     } else if ( request.url.startsWith( '/js' ) ) {
-        listDir( `${baseDir}/${request.url}`, response );
+        const type = ( request.url.endsWith('html') ? 'text/html' : 'text/javascript' );
+        listDir( `${baseDir}/${request.url}`, response, type );
         return;
     }
 
@@ -45,17 +50,19 @@ const server = http.createServer( ( request, response ) => {
     } ).pipe( response );
 } );
 
-const ipaddr = childProcess.execSync( 'ifconfig | grep inet | grep Bcast' ).toString();
-
-const filtered = ipaddr.split( ' ' ).filter( item => {
-    return ( item.trim().length > 0 );
-} ).filter( item => {
-    return ( item.indexOf( 'addr' ) > -1 );
-} ).map( item => item.replace( /addr\:/, '' ) );
-
 const port = 20000;
-console.log( filtered );
-
 server.listen( port );
+try {
+    const ipaddr = childProcess.execSync( 'ifconfig | grep inet | grep Bcast' ).toString();
 
-console.log( `Server listening on port ${port} and IP ${filtered[0]}.` );
+    const filtered = ipaddr.split( ' ' ).filter( item => {
+        return ( item.trim().length > 0 );
+    } ).filter( item => {
+        return ( item.indexOf( 'addr' ) > -1 );
+    } ).map( item => item.replace( /addr\:/, '' ) );
+
+    console.log( filtered );
+    console.log( `Server listening on port ${port} and IP ${filtered[0]}.` );
+} catch ( e ) {
+    console.log( `Server listening on port ${port}.` );
+}
