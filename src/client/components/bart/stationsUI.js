84728bbb66572d6ccb251b52366666b2d9915ce3
Joe <josepha48@gmail.com>
2019-02-15 21:33:54 -0800

    add time stamp for storage

--- src/client/components/bart/stationsUI.js
+++ src/client/components/bart/stationsUI.js
@@ -1,7 +1,10 @@
 import * as API from 'client/components/bart/api';
 import * as Storage from 'client/browser/webStorage';
 
-const STORAGE_KEY = 'SimpleBARTAPIStationList';
+const ONE_WEEK = 604800;
+
+const STORAGE_KEY = 'SimpleBARTAPIStationList', 
+    STORAGE_TIME_KEY = 'SimpleBARTAPIStationListTime';
 
 export default async function StationList() {
 
@@ -9,10 +12,18 @@ export default async function StationList() {
 
     if ( Storage.localEnabled ) {
         stations = Storage.localStore.getItem( 'STORAGE_KEY' );
+        const updateDate = Storage.localStore.getItem( 'STORAGE_TIME_KEY' );
+        const staleDate = new Date().getTime() - ONE_WEEK;
+        if ( !updateDate || updateDate < staleDate ) {
+             stations = [];
+        }
     }
 
     if ( !stations || stations.length === 0 ) {
         stations = await API.getStations();
+        if ( Storage.localEnabled ) {
+            Storage.localStore.setItem( 'STORAGE_TIME_KEY', new Date().getTime() );
+        }
     }
 
     if ( !stations || stations.length === 0 ) {
