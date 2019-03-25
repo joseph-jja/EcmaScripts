73d337138d747a4ca60dd5933c5bbf4a31319340
Joe Acosta <joe.acosta@sephora.com>
2019-03-25 12:11:29 -0700

    move code into utils

--- src/client/components/tasks/taskList.js
+++ src/client/components/tasks/taskList.js
@@ -12,52 +12,13 @@ import {
     getTableRow
 } from 'client/components/tasks/tableFunctions';
 
+import {
+    mapFromDom,
+    tableSort,
+    cancelButtonClick
+} from 'client/components/tasks/taskUtils';
+
 let tasks;
-
-function mapFromDom() {
-
-    const completed = selector( '#completed' ).get( 0 ).checked;
-
-    const options = {
-        'completed': completed,
-        'work_date': dom.html( '#work_date' ),
-        'short_description': dom.html( '#short_description' ),
-        'long_description': dom.html( '#long_description' )
-    };
-
-    return options;
-}
-
-function tableSort() {
-    const tbody = selector( '#taskList tbody' ).get( 0 );
-    const rows = selector( '#taskList tbody tr' ).toArray();
-    const sortedRows = rows.sort( ( a, b ) => {
-        const tdDateA = a.childNodes[ 3 ].innerHTML,
-            tdDateB = b.childNodes[ 3 ].innerHTML;
-
-        const timeA = new Date( tdDateA ).getTime(),
-            timeB = new Date( tdDateB ).getTime();
-
-        const delta = ( timeA - timeB );
-        if ( delta > 0 ) {
-            tbody.prepend( a );
-        } else {
-            tbody.prepend( b );
-        }
-
-        return delta;
-    } );
-}
-
-function cancelButtonClick() {
-
-    const saveButton = selector( '#saveTask' ).get( 0 ),
-        cancelButton = selector( '#cancelTask' ).get( 0 );
-
-    events.removeEvent( saveButton, 'click' );
-    events.removeEvent( cancelButton, 'click' );
-    window.location.reload();
-}
 
 function TaskList() {
 
