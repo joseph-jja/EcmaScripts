b2fb87515771393c73faa02ab8264c60396154ba
Joe <josepha48@gmail.com>
2019-04-01 20:13:54 -0700

    calculate points on curve

--- src/client/workers/star-system.js
+++ src/client/workers/star-system.js
@@ -1,1 +1,18 @@
+import MF from 'utils/mathFunctions';
+
+let center, 
+    resultPoints;
+
+onmessage = (msg) => {
+  
+  if ( msg.setWidthHeight ) {
+    const width = msg.setWidthHeight[0],
+        height = msg.setWidthHeight[1];
+    
+    const radius = MF.getRectangleCorner( width, height );
+
+     resultPoints = MF.getCirclePoints( radius );
+  }
+  
+};
 
