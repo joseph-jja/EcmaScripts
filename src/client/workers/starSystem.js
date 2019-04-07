3e9179697587a38a80045ea3cbb3bfd52145e11a
Joe <josepha48@yahoo.com>
2019-04-06 23:05:29 -0700

    size and location changes

--- src/client/workers/starSystem.js
+++ src/client/workers/starSystem.js
@@ -20,12 +20,12 @@ function getStars( points, hider ) {
     return [ {
         x: ( MF.subtract( center[ 0 ], points.x ) ),
         y: ( MF.subtract( center[ 1 ], points.y ) ),
-        diameter: ( hider ? 19 : 18 ),
+        diameter: ( hider ? 25 : 24 ),
         color: color
     }, {
         x: ( MF.add( center[ 0 ], points.x ) ),
         y: ( MF.add( center[ 1 ], points.y ) ),
-        diameter: ( hider ? 16 : 15 ),
+        diameter: ( hider ? 19 : 18 ),
         color: color
     } ];
 }
@@ -50,17 +50,17 @@ onmessage = ( msg ) => {
 
         center = MF.getRectangleCenter( width, height );
 
-        const radius = MF.getRectangleCorner( width, height );
-        const planetRadius = Math.floor( MF.divide( radius, 3 ) );
+        const radius = MF.divide( MF.getRectangleCorner( width, height ), 1.25 );
+        const planetRadius = Math.floor( MF.divide( radius, ( width > 600 ? 3 : 1.65 ) ) );
 
         resultPoints = MF.getCirclePoints( radius );
-        planetPoints = MF.getCirclePoints( MF.add( planetRadius, ( width > 600 ? 0 : 5 ) ) );
-        sPlanetPoints = MF.getCirclePoints( MF.add( planetRadius, 25 ) );
+        planetPoints = MF.getCirclePoints( MF.add( planetRadius, 0 ) );
+        sPlanetPoints = MF.getCirclePoints( MF.add( planetRadius, ( width > 600 ? 28 : 17 ) ) );
 
         const stars = getStars( resultPoints[ startPoint ], false );
 
-        const planet = getPlanet( stars[ 0 ].x, stars[ 0 ].y, planetPoints[ planetPoint ], false, 5 ),
-            sPlanet = getPlanet( stars[ 0 ].x, stars[ 0 ].y, sPlanetPoints[ sPlanetPoint ], false, 9 );
+        const planet = getPlanet( stars[ 0 ].x, stars[ 0 ].y, planetPoints[ planetPoint ], false, 3 ),
+            sPlanet = getPlanet( stars[ 0 ].x, stars[ 0 ].y, sPlanetPoints[ sPlanetPoint ], false, 7 );
 
         postMessage( {
             stars: {
@@ -75,8 +75,8 @@ onmessage = ( msg ) => {
 
             const oldPoint = resultPoints[ startPoint ];
             const black = getStars( oldPoint, true );
-            const blackPlanet = getPlanet( black[ 0 ].x, black[ 0 ].y, planetPoints[ planetPoint ], true, 5 );
-            const sBlackPlanet = getPlanet( black[ 0 ].x, black[ 0 ].y, sPlanetPoints[ sPlanetPoint ], true, 9 );
+            const blackPlanet = getPlanet( black[ 0 ].x, black[ 0 ].y, planetPoints[ planetPoint ], true, 3 );
+            const sBlackPlanet = getPlanet( black[ 0 ].x, black[ 0 ].y, sPlanetPoints[ sPlanetPoint ], true, 7 );
             startPoint = ( startPoint >= 360 ? 0 : ++startPoint );
             planetPoint = ( planetPoint >= 360 ? 0 : ++planetPoint );
             planetPoint = ( planetPoint >= 360 ? 0 : ++planetPoint );
@@ -85,8 +85,8 @@ onmessage = ( msg ) => {
             sPlanetPoint = ( sPlanetPoint >= 360 ? 0 : ++sPlanetPoint );
             const newPoint = resultPoints[ startPoint ];
             const white = getStars( newPoint, false );
-            const shownPlanet = getPlanet( white[ 0 ].x, white[ 0 ].y, planetPoints[ planetPoint ], false, 5 );
-            const sShownPlanet = getPlanet( white[ 0 ].x, white[ 0 ].y, sPlanetPoints[ sPlanetPoint ], false, 9 );
+            const shownPlanet = getPlanet( white[ 0 ].x, white[ 0 ].y, planetPoints[ planetPoint ], false, 3 );
+            const sShownPlanet = getPlanet( white[ 0 ].x, white[ 0 ].y, sPlanetPoints[ sPlanetPoint ], false, 7 );
 
             postMessage( {
                 stars: {
