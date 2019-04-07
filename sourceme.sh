13585088a1315f7968b1fb982c914b698263dd1b
Joe <josepha48@yahoo.com>
2019-04-07 12:56:29 -0700

    update to spit out what is installed and location of java for yuicompressor

--- sourceme.sh
+++ sourceme.sh
@@ -6,3 +6,13 @@ else
         export BINARY_STELLAR_SYSTEM_HOME=$HOME/binary-stellar-system.github.io/
     fi 
 fi 
+echo "BINARY_STELLAR_SYSTEM_HOME set to $BINARY_STELLAR_SYSTEM"
+
+JAVA_EXEC=`which java`
+ECJ_EXEC=`which ecj`
+if [ "$JAVA_EXE" ] ; then 
+    export JAVA_EXECUTABLE=$JAVA_EXEC
+fi
+echo "JAVA_EXECUTABLE set to $JAVA_EXECUTABLE"
+
+
