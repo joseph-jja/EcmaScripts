6d858482e40b8814ba5ade4aca390d815040b616
Joe Acosta <joe.acosta@sephora.com>
2019-04-08 13:27:35 -0700

    update sourceme file for css compressor

--- sourceme.sh
+++ sourceme.sh
@@ -4,13 +4,17 @@ if [ -d "$HOME/workspace/binary-stellar-system.github.io" ] ; then
 else 
     if [ -d "$HOME/binary-stellar-system.github.io" ] ; then 
         export BINARY_STELLAR_SYSTEM_HOME=$HOME/binary-stellar-system.github.io/
+    else 
+        if [ -d "$HOME/sourcecode/binary-stellar-system.github.io" ] ; then 
+            export BINARY_STELLAR_SYSTEM_HOME=$HOME/sourcecode/binary-stellar-system.github.io/
+        fi 
     fi 
 fi 
-echo "BINARY_STELLAR_SYSTEM_HOME set to $BINARY_STELLAR_SYSTEM"
+echo "BINARY_STELLAR_SYSTEM_HOME set to $BINARY_STELLAR_SYSTEM_HOME"
 
 JAVA_EXEC=`which java`
 ECJ_EXEC=`which ecj`
-if [ "$JAVA_EXE" ] ; then 
+if [ -f "$JAVA_EXEC" ]; then 
     export JAVA_EXECUTABLE=$JAVA_EXEC
 fi
 echo "JAVA_EXECUTABLE set to $JAVA_EXECUTABLE"
