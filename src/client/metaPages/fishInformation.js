06d56700643439357857cff81f4190867a04e45e
Joe Acosta <josepha48@gmail.com>
2019-04-19 11:38:29 -0700

    add in animated fish worker

--- src/client/metaPages/fishInformation.js
+++ src/client/metaPages/fishInformation.js
@@ -1,2 +1,56 @@
+import * as dom from 'client/dom/DOM';
+import selector from 'client/dom/selector';
+import fetcher from 'client/net/fetcher';
+
+import {
+    exists
+} from 'utils/typeCheck';
+
 //mouth.attributes.ry.value = y;
 //omouth.attributes.ry.value = y-4;
+
+let fishInfoWorker;
+
+function setupFishInfo() {
+    fishInfoWorker = exists( Worker ) ? new Worker( '/js/animatedFish.js' ) : undefined;
+}
+
+function startFishInfo() {
+
+    if ( !fishInfoWorker ) {
+        setupFishInfo();
+    }
+
+    if ( fishInfoWorker ) {
+        const homeContent = selector( '#welcome-content' );
+        const displayWindow = selector( '.WebWindowArea', homeContent );
+
+        Array.toArray( displayWindow.childNodes ).forEach( item => {
+            item.style.display = 'none';
+        } );
+
+        const animFishContainer = dom.createElement( 'div', displayWindow, {
+            id: 'animated-fish'
+        } );
+
+        fishInfoWorker.onmessage = ( msg ) => {
+
+        };
+        fishInfoWorker.postMessage( {
+            'start': 'start'
+        } );
+    }
+}
+
+function stopFishInfo() {
+    if ( fishInfoWorker ) {
+        fishInfoWorker.postMessage( {
+            'stop': 'stop'
+        } );
+    }
+}
+
+export {
+    startFishInfo,
+    stopFishInfo
+};
