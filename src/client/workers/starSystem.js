5a721da73c2c0308723ae4fe3b535e58fae65305
Joe Acosta <josepha48@gmail.com>
2019-04-19 16:16:18 -0700

    add 3rd planet

--- src/client/workers/starSystem.js
+++ src/client/workers/starSystem.js
@@ -7,9 +7,11 @@ let center,
     resultPoints,
     planetPoints,
     sPlanetPoints,
+    ePlanetPoints,
     startPoint = 0,
     planetPoint = 0,
     sPlanetPoint = 0,
+    ePlanetPoint = 225,
     timerID;
 
 function getStars( points, hider ) {
@@ -59,17 +61,22 @@ onmessage = ( msg ) => {
         const xradius = MF.add( pradius, ( width > 600 ? 50 : 8 ) );
         sPlanetPoints = MF.getEllipsePoints( xradius, pradius );
 
+        const ePradius = MF.add( radius, 75 );
+        const exradius = MF.add( ePradius, 55 );
+        ePlanetPoints = MF.getEllipsePoints( ePradius, exradius );
+
         const stars = getStars( resultPoints[ startPoint ], false );
 
         const planet = getPlanet( stars[ 0 ].x, stars[ 0 ].y, planetPoints[ planetPoint ], false, 3 ),
-            sPlanet = getPlanet( stars[ 0 ].x, stars[ 0 ].y, sPlanetPoints[ sPlanetPoint ], false, 7 );
+            sPlanet = getPlanet( stars[ 0 ].x, stars[ 0 ].y, sPlanetPoints[ sPlanetPoint ], false, 7 ),
+            ePlanet = getPlanet( center[ 0 ], center[ 1 ], ePlanetPoints[ ePlanetPoint ], false, ( width > 600 ? 6 : 0 ) );
 
         postMessage( {
             stars: {
                 white: stars
             },
             planets: {
-                shownPlanet: planet.concat( sPlanet )
+                shownPlanet: planet.concat( sPlanet, ePlanet )
             }
         } );
 
@@ -79,21 +86,28 @@ onmessage = ( msg ) => {
             const black = getStars( oldPoint, true );
             const blackPlanet = getPlanet( black[ 0 ].x, black[ 0 ].y, planetPoints[ planetPoint ], true, 3 );
             const sBlackPlanet = getPlanet( black[ 0 ].x, black[ 0 ].y, sPlanetPoints[ sPlanetPoint ], true, 7 );
+            const eBlackPlanet = getPlanet( center[ 0 ], center[ 1 ], ePlanetPoints[ ePlanetPoint ], true, ( width > 600 ? 6 : 0 ) );
             startPoint = ( startPoint <= 0 ? 360 : --startPoint );
-            if ( planetPoint < 2 ) {
+            if ( planetPoint < 4 ) {
                 planetPoint = 360;
             } else {
-                planetPoint -= 3;
+                planetPoint -= 4;
             }
-            if ( sPlanetPoint < 1 ) {
+            if ( sPlanetPoint < 3 ) {
                 sPlanetPoint = 360;
             } else {
-                sPlanetPoint -= 2;
+                sPlanetPoint -= 3;
+            }
+            if ( ePlanetPoint < 2 ) {
+                ePlanetPoint = 360;
+            } else {
+                ePlanetPoint -= 2;
             }
             const newPoint = resultPoints[ startPoint ];
             const white = getStars( newPoint, false );
             const shownPlanet = getPlanet( white[ 0 ].x, white[ 0 ].y, planetPoints[ planetPoint ], false, 3 );
             const sShownPlanet = getPlanet( white[ 0 ].x, white[ 0 ].y, sPlanetPoints[ sPlanetPoint ], false, 7 );
+            const eShownPlanet = getPlanet( center[ 0 ], center[ 1 ], ePlanetPoints[ ePlanetPoint ], false, ( width > 600 ? 6 : 0 ) );
 
             postMessage( {
                 stars: {
@@ -101,8 +115,8 @@ onmessage = ( msg ) => {
                     white
                 },
                 planets: {
-                    blackPlanet: blackPlanet.concat( sBlackPlanet ),
-                    shownPlanet: shownPlanet.concat( sShownPlanet )
+                    blackPlanet: blackPlanet.concat( sBlackPlanet, eBlackPlanet ),
+                    shownPlanet: shownPlanet.concat( sShownPlanet, eShownPlanet )
                 }
             } );
 
