ee12558ec1af974267d1744b42dd35de2fcd361f
Joe Acosta <josepha48@gmail.com>
2019-04-19 10:22:21 -0700

    add animated fish worker

---
+++ src/client/workers/animatedFish.js
@@ -0,0 +1,29 @@
+import MF from 'utils/mathFunctions';
+
+let y = 5,
+    direction = 1,
+    timerID;
+
+onmessage = ( msg ) => {
+
+    if ( msg && msg.data && msg.data.start ) {
+        timerID = setInterval( () => {
+            if ( direction ) {
+                y++;
+            } else {
+                y--;
+            }
+            if ( y > 15 ) {
+                direction = 0;
+            } else if ( y < 6 ) {
+                direction = 1;
+            }
+            postMessage( {
+                mouth: y,
+                omouth: MF.subtract( y, 4 )
+            } );
+        }, 100 );
+    } else if ( msg && msg.data && msg.data.stop ) {
+        clearInterva( timerID );
+    }
+}
