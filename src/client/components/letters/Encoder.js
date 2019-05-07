ae453b58b27cd87c987fd9f9de86faa97e6616b1
Joe <josepha48@yahoo.com>
2019-05-07 17:47:46 +0200

    add base 10 field and update

--- src/client/components/letters/Encoder.js
+++ src/client/components/letters/Encoder.js
@@ -2,6 +2,9 @@ import selector from 'client/dom/selector';
 import * from dom from 'client/dom/DOM';
 import * from events from 'client/dom/events';
 import Grid from 'client/components/letters/Grid';
+import {
+    convertFromBaseXToBaseTen
+} from 'util/mathFunctions';
 
 //the character render engine
 //this can be used to create a character on the grid
@@ -27,9 +30,15 @@ export default class Encoder extends Grid {
         } );
 
         selector( 'tr', this.tableID ).each( row => {
-            const cell = dom.createElement( 'td', row, {} );
-            const inp = dom.createElement( 'input', cell, {
+            let cell = dom.createElement( 'td', row, {} );
+            let inp = dom.createElement( 'input', cell, {
                 id: `${row.id}_inp`,
+                type: 'text',
+                size: 8
+            } );
+            cell = dom.createElement( 'td', row, {} );
+            inp = dom.createElement( 'input', cell, {
+                id: `${row.id}_inp_hex`,
                 type: 'text',
                 size: 8
             } );
@@ -40,10 +49,14 @@ export default class Encoder extends Grid {
             if ( tgt.nodeName().toLowerCase() === 'input' && x.type === 'checkbox' ) {
                 const rowID = tgt.parentNode().id;
                 const eId = document.getElementById( `${rowId}_inp` ),
+                    hexId = document.getElementById( `${rowId}_inp_hex` ),
                     val = ( tgt.checked || 0 );
 
                 let cval = eId.value || '';
                 eId.value = `${oval}${val}`;
+
+                const binVal = convertFromBaseXToBaseTen( 2, eId.value );
+                hexId.value = binVal;
             }
         } );
     }
