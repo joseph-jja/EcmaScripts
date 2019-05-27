d24af8037c44428e6ef486596b9fbe323ed80378
Joe <josepha48@yahoo.com>
2019-05-26 17:39:04 -0700

    update station list code to be more reusable

---
+++ src/client/components/bart/stationsList.js
@@ -0,0 +1,38 @@
+import * as API from 'client/components/bart/api';
+import * as Storage from 'client/browser/webStorage';
+
+const ONE_WEEK = 604800;
+
+const STORAGE_KEY = 'SimpleBARTAPIStationList',
+    STORAGE_TIME_KEY = 'SimpleBARTAPIStationListTime';
+
+export default async function StationList() {
+
+    let stations = [];
+
+    if ( Storage.localEnabled ) {
+        stations = Storage.localStore.getItem( 'STORAGE_KEY' );
+        const updateDate = Storage.localStore.getItem( 'STORAGE_TIME_KEY' );
+        const staleDate = new Date().getTime() - ONE_WEEK;
+        if ( !updateDate || updateDate < staleDate ) {
+            stations = [];
+        }
+    }
+
+    if ( !stations || stations.length === 0 ) {
+        stations = await API.getStations();
+        if ( Storage.localEnabled ) {
+            Storage.localStore.setItem( 'STORAGE_TIME_KEY', new Date().getTime() );
+        }
+    }
+
+    if ( !stations || stations.length === 0 ) {
+        throw ( 'Could not get stations!' );
+    }
+
+    if ( Storage.localEnabled ) {
+        Storage.localStore.setItem( STORAGE_KEY, JSON.stringify( stations ) );
+    }
+
+    return stations;
+}
--- src/client/components/bart/stationsUI.js
+++ src/client/components/bart/stationsUI.js
@@ -1,38 +1,8 @@
-import * as API from 'client/components/bart/api';
-import * as Storage from 'client/browser/webStorage';
+import StationsList from 'client/components/bart/stationsList';
 
-const ONE_WEEK = 604800;
+export default async function Stations() {
 
-const STORAGE_KEY = 'SimpleBARTAPIStationList',
-    STORAGE_TIME_KEY = 'SimpleBARTAPIStationListTime';
-
-export default async function StationList() {
-
-    let stations = [];
-
-    if ( Storage.localEnabled ) {
-        stations = Storage.localStore.getItem( 'STORAGE_KEY' );
-        const updateDate = Storage.localStore.getItem( 'STORAGE_TIME_KEY' );
-        const staleDate = new Date().getTime() - ONE_WEEK;
-        if ( !updateDate || updateDate < staleDate ) {
-            stations = [];
-        }
-    }
-
-    if ( !stations || stations.length === 0 ) {
-        stations = await API.getStations();
-        if ( Storage.localEnabled ) {
-            Storage.localStore.setItem( 'STORAGE_TIME_KEY', new Date().getTime() );
-        }
-    }
-
-    if ( !stations || stations.length === 0 ) {
-        throw ( 'Could not get stations!' );
-    }
-
-    if ( Storage.localEnabled ) {
-        Storage.localStore.setItem( STORAGE_KEY, JSON.stringify( stations ) );
-    }
+    const stations = await StationsList();
 
     const items = stations.map( station => {
 
