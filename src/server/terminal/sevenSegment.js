096d7f2cb4d04d790d91ac6885376522f0c48c69
Joe Acosta <josepha48@gmail.com>
2019-09-27 14:50:53 -0700

    create a seven segment code

--- src/server/terminal/sevenSegment.js
+++ src/server/terminal/sevenSegment.js
@@ -1,24 +1,49 @@
 const baseDir = process.cwd(),
-{
-    clearScreen,
-    clearMatrix,
-    writeMatrix
-} = require( `${baseDir}/src/server/terminal/matrix` ),
-{
-    horizontalSegment,
-    verticleSegment
-} = require( `${baseDir}/src/server/terminal/matrixDisplay` );
+    {
+        clearScreen,
+        clearMatrix,
+        writeMatrix
+    } = require(`${baseDir}/src/server/terminal/matrix`),
+    {
+        horizontalSegment,
+        verticleSegment
+    } = require(`${baseDir}/src/server/terminal/matrixDisplay`);
 
 
-function writeSegment(sevenSegmentItem, segment) {
+function sevenSegment(sevenSegmentItem, segment) {
 
     let column = 1;
-    if (sevenSegment > 0 && sevenSegment <=4) {
+    if (sevenSegment > 0 && sevenSegment <= 4) {
         column = sevenSegment;
     }
     let row = 1;
     switch (segment) {
-      
+        case 1:
+            verticleSegment(1, column, 0);
+            break;
+        case 2:
+            horizontalSegment(1, column);
+            break;
+        case 3:
+            verticleSegment(1, column, 1);
+            break;
+        case 4:
+            horizontalSegment(2, column);
+            break;
+        case 5:
+            verticleSegment(2, column, 0);
+            break;
+        case 6:
+            horizontalSegment(3, column);
+            break;
+        case 7:
+            verticleSegment(2, column, 1);
+            break;
+        default:
+            break;
     }
+}
 
+module.exports = {
+    sevenSegment
 }
