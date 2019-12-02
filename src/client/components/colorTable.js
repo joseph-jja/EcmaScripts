64f859e19d217086168fba5cfa4c31a55d9b14e1
Joe <josepha48@gmail.com>
2019-12-01 18:15:54 -0800

    Remove hsl stuff

--- src/client/components/colorTable.js
+++ src/client/components/colorTable.js
@@ -3,32 +3,6 @@ import {
     convertFromBaseTenToBaseX
 } from 'utils/mathFunctions';
 
-function rgbToHsl( r, g, b ) {
-    const max = Math.max( r, g, b ),
-        min = Math.min( r, g, b );
-    let h, s, l = ( max + min ) / 2;
-
-    if ( max === min ) {
-        h = s = 0; // achromatic
-    } else {
-        let d = max - min;
-        s = l > 0.5 ? d / ( 2 - max - min ) : d / ( max + min );
-        switch ( max ) {
-        case r:
-            h = ( g - b ) / d + ( g < b ? 6 : 0 );
-            break;
-        case g:
-            h = ( b - r ) / d + 2;
-            break;
-        case b:
-            h = ( r - g ) / d + 4;
-            break;
-        }
-        h /= 6;
-    }
-    return new Array( h * 360, s * 100, l * 100 );
-}
-
 function sortColors( rgb1, rgb2 ) {
     const r1 = convertFromBaseXToBaseTen( 16, rgb1.substring( 1, 2 ) ),
         g1 = convertFromBaseXToBaseTen( 16, rgb1.substring( 2, 3 ) ),
@@ -36,9 +10,6 @@ function sortColors( rgb1, rgb2 ) {
         r2 = convertFromBaseXToBaseTen( 16, rgb2.substring( 1, 2 ) ),
         g2 = convertFromBaseXToBaseTen( 16, rgb2.substring( 2, 3 ) ),
         b2 = convertFromBaseXToBaseTen( 16, rgb2.substring( 3, 4 ) );
-
-    const hsl1 = rgbToHsl( ( r1 * 17) / 255, ( g1 * 17) / 255, ( b1 * 17) / 255 )[ 0 ],
-        hsl2 = rgbToHsl( ( r2 * 17) / 255, ( g2 * 17) / 255, ( b2 * 17) / 255)[ 0 ];
 
     const res1 = ( +r1 + +g1 + +b1), 
         res2 = ( +r2 + +g2 + +b2);
@@ -48,7 +19,7 @@ function sortColors( rgb1, rgb2 ) {
         max2 = Math.max( r2, g2, b2 ),
         min2 = Math.min( r2, g2, b2 );
 
-    return hsl2 - hsl1;
+    return res2 - res1;
 }
 
 function getHexValues() {
