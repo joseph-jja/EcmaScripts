9ac01dc4be65f7a3c74fc4a54133f84ae595d0fe
Joe Acosta <joe.acosta@sephora.com>
2019-12-05 10:20:59 -0800

    update overlay impl

--- src/client/components/overlay.js
+++ src/client/components/overlay.js
@@ -1,8 +1,29 @@
 import selector from 'client/dom/selector';
+import * as css from 'client/dom/CSS';
+import * as dom from 'client/dom/DOM';
+import * as events from 'client/dom/events';
 
-function createOverlay( options ) {
+function createOverlay( options, content ) {
 
+    const overlay = dom.createElement( 'div', parent, {
+        'id': options.id || 'overlay'
+    } );
 
+    if ( options.styles ) {
+        Object.keys( options.styles ).forEach( key => {
+            overlay.style[ key ] = options.styles[ key ];
+        } );
+    }
+
+    if ( !overlay.style.width ) {
+        overlay.style.width = '20em';
+    }
+
+    if ( !overlay.style.height ) {
+        overlay.style.height = '4em';
+    }
+
+    return overlay;
 }
 
 export {
