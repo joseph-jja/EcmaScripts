ec9bc74d2830a8c87f3aafdc3771820ae75a0491
Joe <josepha48@gmail.com>
2019-12-31 12:37:14 -0800

    patch fix

--- src/utils/dateFunctions.js
+++ src/utils/dateFunctions.js
@@ -1,3 +1,16 @@
+// normally when dealing with objects in javascript you would set the
+// prototype of that object to the new other object
+// however with javascript base objects, this does not work the same
+
+const DateFunctions = {};
+
+// month names the the last day of the month, short name for week day and also long name if someone wants it
+DateFunctions.monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+DateFunctions.monthShortNames = [ "Jan ", "Feb ", "Mar ", "Apr ", "May ", "Jun ", "Jul ", "Aug ", "Sep ", "Oct ", "Nov ", "Dec " ];
+DateFunctions.numberOfDaysInMonth = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+DateFunctions.weekDayShortNames = [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ];
+DateFunctions.weekDayNames = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+
 // get the first day of the current month
 DateFunctions.getFirstOfMonthDayOfWeek = function ( dIn ) {
     let fomd,
