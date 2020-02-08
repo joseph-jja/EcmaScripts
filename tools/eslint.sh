5b965fecf88dadde1b57585dd0bef54003adde75
joseph-jja <josepha48@gmail.com>
2020-02-08 14:32:47 -0800

    install js-beautify

--- tools/eslint.sh
+++ tools/eslint.sh
@@ -1,6 +1,9 @@
-#! /bin/sh 
+#! /bin/sh
 
-DIR_FILES="*.js src tools tests config"
-for FILES in $DIR_FILES; do 
-    npx eslint --config config/eslint.json $FILES
+DIR_FILES = "*.js src tools tests config"
+for FILES in $DIR_FILES; do
+    #for ALL_FILES in `find $FILES -type f`; do 
+    #    npx js - beautify--config config / js - beautify.json - r $ALL_FILES
+    #done
+    npx eslint--config config / eslint.json $FILES
 done
