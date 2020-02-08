458f525e1b48e03dc75023abcaf3882630c83a84
joseph-jja <josepha48@gmail.com>
2020-02-08 14:04:26 -0800

    beautify

--- terminal-clock.js
+++ terminal-clock.js
@@ -3,11 +3,11 @@ const baseDir = process.cwd(),
         clearScreen,
         clearMatrix,
         writeMatrix
-    } = require(`${baseDir}/src/server/terminal/matrix`),
+    } = require( `${baseDir}/src/server/terminal/matrix` ),
     {
         digits,
         setColon
-    } = require(`${baseDir}/src/server/terminal/digits`);
+    } = require( `${baseDir}/src/server/terminal/digits` );
 
 
 clearScreen();
@@ -15,31 +15,31 @@ clearMatrix();
 
 let now = new Date();
 
-let hours = `${now.getHours()}`.padStart(2, 0);
-let minutes = `${now.getMinutes()}`.padStart(2, 0);
+let hours = `${now.getHours()}`.padStart( 2, 0 );
+let minutes = `${now.getMinutes()}`.padStart( 2, 0 );
 
-digits(1, +hours.substring(0, 1));
-digits(2, +hours.substring(1));
-digits(3, +minutes.substring(0, 1));
-digits(4, +minutes.substring(1));
+digits( 1, +hours.substring( 0, 1 ) );
+digits( 2, +hours.substring( 1 ) );
+digits( 3, +minutes.substring( 0, 1 ) );
+digits( 4, +minutes.substring( 1 ) );
 setColon();
 
 writeMatrix();
 
 //setTimeout(() => {
-setInterval(() => {
+setInterval( () => {
     clearMatrix();
 
     now = new Date();
-    hours = `${now.getHours()}`.padStart(2, 0);
-    minutes = `${now.getMinutes()}`.padStart(2, 0);
+    hours = `${now.getHours()}`.padStart( 2, 0 );
+    minutes = `${now.getMinutes()}`.padStart( 2, 0 );
 
-    digits(1, +hours.substring(0, 1));
-    digits(2, +hours.substring(1));
-    digits(3, +minutes.substring(0, 1));
-    digits(4, +minutes.substring(1));
+    digits( 1, +hours.substring( 0, 1 ) );
+    digits( 2, +hours.substring( 1 ) );
+    digits( 3, +minutes.substring( 0, 1 ) );
+    digits( 4, +minutes.substring( 1 ) );
     setColon();
 
     writeMatrix();
 
-}, 1000);
+}, 1000 );
