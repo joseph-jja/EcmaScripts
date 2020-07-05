811460d71a475419db615fefad1dc6a64a567a25
joseph-jja <josepha48@gmail.com>
2020-07-05 16:36:13 -0700

    remove trailing slash

--- sourceme.sh
+++ sourceme.sh
@@ -1,12 +1,12 @@
 
 if [ -d "$HOME/workspace/binary-stellar-system.github.io" ] ; then 
-    export BINARY_STELLAR_SYSTEM_HOME=$HOME/workspace/binary-stellar-system.github.io/
+    export BINARY_STELLAR_SYSTEM_HOME=$HOME/workspace/binary-stellar-system.github.io
 else 
     if [ -d "$HOME/binary-stellar-system.github.io" ] ; then 
-        export BINARY_STELLAR_SYSTEM_HOME=$HOME/binary-stellar-system.github.io/
+        export BINARY_STELLAR_SYSTEM_HOME=$HOME/binary-stellar-system.github.io
     else 
         if [ -d "$HOME/sourcecode/binary-stellar-system.github.io" ] ; then 
-            export BINARY_STELLAR_SYSTEM_HOME=$HOME/sourcecode/binary-stellar-system.github.io/
+            export BINARY_STELLAR_SYSTEM_HOME=$HOME/sourcecode/binary-stellar-system.github.io
         fi 
     fi 
 fi 
