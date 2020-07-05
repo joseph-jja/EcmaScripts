c5758f50ef8a0555ad15b643c6f604002dc5d106
joseph-jja <josepha48@gmail.com>
2020-07-05 16:38:52 -0700

    eslint script for all the files

--- tools/eslint.sh
+++ tools/eslint.sh
@@ -2,8 +2,5 @@
 
 DIR_FILES = "*.js src tools tests config"
 for FILES in $DIR_FILES; do
-    #for ALL_FILES in `find $FILES -type f`; do 
-    #    npx js - beautify--config config / js - beautify.json - r $ALL_FILES
-    #done
     npx eslint--config config / eslint.json $FILES
 done
