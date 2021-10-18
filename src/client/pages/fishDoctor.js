84f7c857a8dba1da1d475df38c01c3fc23e6f491
Joe Acosta <joe.acosta@sephora.com>
2021-10-18 14:50:30 -0700

    add in framework for fish doctor page

---
+++ src/client/pages/fishDoctor.js
@@ -0,0 +1,26 @@
+import * as dom from 'client/dom/DOM';
+import {
+    addOnLoad
+} from 'client/dom/events';
+
+import WebWindow from 'client/components/wbWindow';
+import * as canvas from 'client/components/canvas';
+
+function generateFish( )
+{
+
+    const mw = document.getElementById( 'main-window' );
+    const styles = window.getComputedStyle( mw );
+
+    const webWin = new WebWindow( 'Canvas Test',
+        styles.offsetLeft,
+        styles.offsetTop,
+        styles.offsetWidth,
+        styles.offsetHeight,
+        'main-window' );
+
+    const res = canvas.create( "canvas-test", "canvas-container", 800, 500 );
+    window.canvasRef = res;
+}
+
+addOnLoad( generateFish );
