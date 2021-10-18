c3dd15ad671b720f320fb488f74433c605852825
Joe Acosta <joe.acosta@sephora.com>
2021-10-18 15:49:36 -0700

    add in fishes eye :)

--- src/client/pages/fishDoctor.js
+++ src/client/pages/fishDoctor.js
@@ -6,21 +6,28 @@ import {
 import WebWindow from 'client/components/wbWindow';
 import * as canvas from 'client/components/canvas';
 
-function generateFish( )
-{
+function generateFish() {
 
-    const mw = document.getElementById( 'main-window' );
-    const styles = window.getComputedStyle( mw );
+    const mw = document.getElementById('main-window');
+    const styles = window.getComputedStyle(mw);
 
-    const webWin = new WebWindow( 'Canvas Test',
+    const webWin = new WebWindow('Canvas Test',
         styles.offsetLeft,
         styles.offsetTop,
         styles.offsetWidth,
         styles.offsetHeight,
-        'main-window' );
+        'main-window');
 
-    const res = canvas.create( "fish-doctor", "canvas-container", 800, 500 );
+    const res = canvas.create("fish-doctor", "canvas-container", 800, 500);
+    ref.circle(650, 250, 10, {
+        color: 'black',
+        fillStrokeClear: 'fill'
+    });
+    ref.circle(650, 250, 20, {
+        color: 'black'
+    });
+
     window.canvasRef = res;
 }
 
-addOnLoad( generateFish );
+addOnLoad(generateFish);
