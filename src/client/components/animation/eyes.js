ff532ea4611f34bc21f0c4673695c140536b15cb
joseph-jja <josepha48@gmail.com>
2021-10-30 13:28:59 -0700

    update eye code

--- src/client/components/animation/eyes.js
+++ src/client/components/animation/eyes.js
@@ -1,24 +1,28 @@
 export function setupEyes( canvas ) {
 
-    canvas.createEye = function ( xIrisCenter, yIrisCEnter, xCenter, yCenter, options = {} ) {
+    canvas.createEye = function ( xIrisCenter, yIrisCenter, xCenter, yCenter, options = {} ) {
 
         const eyeOptions = Object.assign( {}, options );
-
-        // handle iris
-        canvas.circle( xIrisCenter, yIrisCenter, eyeOptions.irisRadius || 10, eyeOptions );
-
-        // handle pupal
-        canvas.circle( xIrisCenter, yIrisCenter, eyeOptions.pupalRadius || 15, eyeOptions );
-
+        
         // handle white of the eye
+        eyeOptions.color = eyeOptions.eyeColor || 'white';
         eyeOptions.shape = ( options.shape &&
             ( options.shape === 'circle' || options.shape === 'oval' ) ?
-            options.shape : ircle );
+            options.shape : 'circle' );
 
         if ( eyeOptions.shape === 'oval' && eyeOptions.eyeWidth && eyeOptions.eyeHeight ) {
             canvas.oval( xCenter, yCenter, eyeOptions.eyeWidth, eyeOptions.eyeHeight, eyeOptions );
         } else {
             canvas.circle( xCenter, yCenter, eyeOptions.eyeSize || 30, eyeOptions );
         }
+
+        // handle pupal
+        eyeOptions.color = eyeOptions.pupalColor || 'brown';
+        eyeOptions.fillStrokeClear = eyeOptions.pupalColor || 'brown';
+        canvas.circle( xIrisCenter, yIrisCenter, eyeOptions.pupalRadius || 15, eyeOptions );
+
+        // handle iris
+        eyeOptions.color = eyeOptions.irisColor || 'black';
+        canvas.circle( xIrisCenter, yIrisCenter, eyeOptions.irisRadius || 10, eyeOptions );
     }
 }
