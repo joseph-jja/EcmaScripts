6a826f1ad0fddbae53650faddb98c6d14f1ce728
Joe Acosta <joe.acosta@sephora.com>
2022-01-04 11:09:51 -0800

    update browser deets

--- src/client/browser/useragentParser.js
+++ src/client/browser/useragentParser.js
@@ -1,8 +1,11 @@
 import dtct from "client/browser/detect";
-import browserList as browser from "client/browser/browserList";
+import browserList from "client/browser/browserList";
+import osList from "client/browser/osList";
 
-let os = [],
-    uaCleaned, rules = [],
+const browser = browserList,
+    os = osList;
+
+let uaCleaned, rules = [],
     result,
     detect = dtct();
 
@@ -45,139 +48,6 @@ function parse( map, cb, uaString ) {
         "version": version
     };
 };
-
-// jsmeter says that arrays of objects are complex at the 'program level'
-//	map of main browsers
-// jsmeter says that arrays of objects are complex at the 'program level'
-//	map of main browsers
-
-
-// map of main os
-os = [ {
-    search: "media center pc",
-    name: "Microsoft Media Center PC",
-    version: "media center pc"
-}, {
-    search: "windows nt",
-    name: "Microsoft Windows",
-    version: "windows nt"
-}, {
-    search: "windows phone",
-    name: "Microsoft Windows Phone OS",
-    version: "iemobile"
-}, {
-    search: "windows ce",
-    name: "Microsoft Windows CE",
-    version: "iemobile"
-}, {
-    search: "windows",
-    name: "Microsoft Windows",
-    version: "windows"
-}, {
-    search: "android",
-    name: "Android",
-    version: "android"
-}, {
-    search: "ipod",
-    name: "iPod",
-    version: "iphone os"
-}, {
-    search: "iphone",
-    name: "iPhone",
-    version: "iphone os"
-}, {
-    search: "playbook",
-    name: "Blackberry Playbook",
-    version: "blakberry"
-}, {
-    search: "blackberry",
-    name: "Blackberry",
-    version: "blackbery"
-}, {
-    search: "playstation",
-    name: "Playstation",
-    version: "playstation"
-}, {
-    search: "hp-tablet",
-    name: "HP Touchpad",
-    version: "hp-tablet"
-}, {
-    search: "hpwos",
-    name: "HP Touchpad",
-    version: "hpwos"
-}, {
-    search: "nintendo wii",
-    name: "Nintendo Wii",
-    version: "nintendo wii"
-}, {
-    search: "nintendo 3ds",
-    name: "Nintendo 3DS",
-    version: "nintendo 3ds"
-}, {
-    search: "fedora",
-    name: "Fedora",
-    version: "fedora"
-}, {
-    search: "centos",
-    name: "Cent OS",
-    version: "centos"
-}, {
-    search: "ubuntu",
-    name: "ubuntu",
-    version: "ubuntu"
-}, {
-    search: "linux mint",
-    name: "Linux Mint",
-    version: "linux mint"
-}, {
-    search: "ipad",
-    name: "iPad",
-    version: "iphone os"
-}, {
-    search: "ipad",
-    name: "iPad",
-    version: "cpu os"
-}, {
-    search: "macintosh",
-    name: "Macintosh",
-    version: "mac os x"
-}, {
-    search: "cros",
-    name: "Chrome OS",
-    version: "cros i686"
-}, {
-    search: "webos",
-    name: "Web OS",
-    version: "webos"
-}, {
-    search: "symbian",
-    name: "Symbian",
-    version: "symbian"
-}, {
-    search: "sunos",
-    name: "Sun OS",
-    version: "sunos"
-}, {
-    search: "samsung",
-    name: "Unknown Samsung",
-    version: "samsung"
-}, {
-    search: "pantech",
-    name: "Unknown Pantech",
-    version: "pantech"
-}, {
-    search: "darwin",
-    name: "Unknown Darwin",
-    version: "darwin"
-}, {
-    search: "linux",
-    name: "Unknown Linux",
-    version: "linux"
-}, {
-    search: "win9",
-    name: "Microsoft Windows",
-    version: "win9"
-} ];
 
 rules[ "os" ] = function ( idx, map, name, version ) {
 
--- tests/client/browser/useragentParser_spec.js
+++ tests/client/browser/useragentParser_spec.js
@@ -5,16 +5,16 @@ describe( 'browser tests', function () {
 
     it( 'detect tests', () => {
         var d = detect();
-        expect( d ).not.toBe( undefined );
+        expect( d ).toBeDefined();
     } );
 
     // FYI phantomjs is all over the place, so we just make sure we get values
     it( 'useragent tests', () => {
         var d = useragentParser;
-        expect( d ).not.toBe( undefined );
-        expect( d.uaOS ).not.toBe( undefined );
-        expect( d.uaName ).not.toBe( undefined );
-        expect( d.uaOSVersion ).not.toBe( undefined );
-        expect( d.uaAppVersion ).not.toBe( undefined );
+        expect( d ).toBeDefined();
+        expect( d.uaOS ).toBeDefined();
+        expect( d.uaName ).toBeDefined();
+        expect( d.uaOSVersion ).toBeDefined();
+        expect( d.uaAppVersion ).toBeDefined();
     } );
 } );
