c3507f7f601948a73e262e65418b3cd930b87fc6
Joe Acosta <joe.acosta@sephora.com>
2022-04-28 15:23:34 -0700

    complete class object

--- src/client/db/IndexedDB.js
+++ src/client/db/IndexedDB.js
@@ -1,3 +1,4 @@
+/* eslint class-methods-use-this: 0 */
 class SQLQuery {
 
     constructor( name ) {
@@ -7,6 +8,29 @@ class SQLQuery {
         }
         this.version = 1;
         this.iDB = undefined;
+    }
+
+    getObjectStore( storeName, mode = 'readonly' ) {
+        const tx = this.iDB.transaction( storeName, mode );
+        tx.oncomplete = function ( evt ) {
+            console.log( 'Transaction completed!', evt );
+        };
+        tx.onerror = function ( evt ) {
+            console.error( `Transaction error: ${tx.error}`, evt );
+        };
+        return tx.objectStore( storeName );
+    }
+
+    processRequest( request ) {
+        return new Promise((resolve, reject) => {
+            request.onerror = function ( evt ) {
+                reject( evt );
+            };
+
+            request.onsuccess = function ( evt ) {
+                resolve( evt );
+            };
+        });
     }
 
     hasIndexedDBSupport() {
@@ -24,45 +48,17 @@ class SQLQuery {
             keypath: 'id',
             autoIncrement: true
         } );
-        return processRequest.call( this, request );
+        return this.processRequest( request );
     }
 
     destroyDB( dbName ) {
         // TODO implement?
         window.indexedDB.deleteDatabase( dbName );
     }
-};
+}
 
 SQLQuery.DB_SUCCESS = 200;
 SQLQuery.DB_ERROR = 500;
-
-// DRY the code
-function getObjectStore( db, storeName, mode = 'readonly', txCompletedHandler ) {
-    const tx = db.transaction( storeName, mode );
-    if ( typeof txCompletedHandler === 'function' ) {
-        tx.oncomplete = txCompletedHandler;
-    } else {
-        tx.oncomplete = function ( evt ) {
-            console.log( 'Transaction completed!', evt );
-        };
-    }
-    tx.onerror = function ( evt ) {
-        console.log( `Transaction error: ${tx.error}`, evt );
-    };
-    return tx.objectStore( storeName );
-}
-
-// DRY the code with a promisified version
-function processRequest( request, callback ) {
-
-    request.onerror = function ( evt ) {
-        callback( evt, SQLQuery.DB_ERROR );
-    };
-
-    request.onsuccess = function ( evt ) {
-        callback( evt, SQLQuery.DB_SUCCESS );
-    };
-}
 
 SQLQuery.prototype.open = function ( name, store, version ) {
     return new Promise( ( resolve, reject ) => {
@@ -141,8 +137,12 @@ SQLQuery.prototype.openHandler = function ( storeName, successHandler, errorHand
 
 SQLQuery.prototype.clear = function ( storeName, callback ) {
     const clearTransaction = () => {
-        const request = getObjectStore( this.iDB, storeName, "readwrite" ).clear();
-        processRequest( request, callback );
+        const request = this.getObjectStore( storeName, 'readwrite' ).clear();
+        this.processRequest( request ).then(evt => {
+            callback(evt, SQLQuery.DB_SUCCESS);
+        }).catch(evt => {
+            callback(evt, SQLQuery.DB_ERROR);
+        });
     };
     this.openHandler( storeName, clearTransaction, callback );
 };
@@ -154,11 +154,19 @@ SQLQuery.prototype.add = function ( storeName, data, callback ) {
             const key = data.key;
             const ndata = Object.assign( {}, data );
             delete ndata.key;
-            const request = getObjectStore( this.iDB, storeName, "readwrite" ).add( data, key );
-            processRequest( request, callback );
+            const request = this.getObjectStore( storeName, 'readwrite' ).add( data, key );
+            this.processRequest( request ).then(evt => {
+                callback(evt, SQLQuery.DB_SUCCESS);
+            }).catch(evt => {
+                callback(evt, SQLQuery.DB_ERROR);
+            });
         } else {
-            const request = getObjectStore( this.iDB, storeName, "readwrite" ).add( data );
-            processRequest( request, callback );
+            const request = this.getObjectStore( storeName, 'readwrite' ).add( data );
+            this.processRequest( request ).then(evt => {
+                callback(evt, SQLQuery.DB_SUCCESS);
+            }).catch(evt => {
+                callback(evt, SQLQuery.DB_ERROR);
+            });
         }
     };
     this.openHandler( storeName, addTransaction, callback );
@@ -167,8 +175,12 @@ SQLQuery.prototype.add = function ( storeName, data, callback ) {
 // callback gets the object data and success for fail
 SQLQuery.prototype.fetch = function ( storeName, key, callback ) {
     const fetchTransaction = () => {
-        const request = getObjectStore( this.iDB, storeName, "readonly" ).get( key );
-        processRequest( request, callback );
+        const request = this.getObjectStore( storeName, 'readonly' ).get( key );
+        this.processRequest( request ).then(evt => {
+            callback(evt, SQLQuery.DB_SUCCESS);
+        }).catch(evt => {
+            callback(evt, SQLQuery.DB_ERROR);
+        });
     };
     this.openHandler( storeName, fetchTransaction, callback );
 };
@@ -176,16 +188,24 @@ SQLQuery.prototype.fetch = function ( storeName, key, callback ) {
 // update gets and then updates
 SQLQuery.prototype.update = function ( storeName, key, data, callback ) {
     const updateTransaction = () => {
-        const request = getObjectStore( this.iDB, storeName, "readwrite" ).put( data, key );
-        processRequest( request, callback );
+        const request = this.getObjectStore( storeName, 'readwrite' ).put( data, key );
+        this.processRequest( request ).then(evt => {
+            callback(evt, SQLQuery.DB_SUCCESS);
+        }).catch(evt => {
+            callback(evt, SQLQuery.DB_ERROR);
+        });
     };
     this.openHandler( storeName, updateTransaction, callback );
 };
 
 SQLQuery.prototype.remove = function ( storeName, key, callback ) {
     const removeTransaction = () => {
-        const request = getObjectStore( this.iDB, storeName, "readwrite" ).delete( key );
-        processRequest( request, callback );
+        const request = this.getObjectStore( storeName, 'readwrite' ).delete( key );
+        this.processRequest( request ).then(evt => {
+            callback(evt, SQLQuery.DB_SUCCESS);
+        }).catch(evt => {
+            callback(evt, SQLQuery.DB_ERROR);
+        });
     };
     this.openHandler( storeName, removeTransaction, callback );
 };
@@ -194,7 +214,7 @@ SQLQuery.prototype.list = function ( storeName, callback ) {
     const listTransaction = () => {
         const lb = window.IDBKeyRange.lowerBound( 0 );
 
-        const store = getObjectStore( this.iDB, storeName, "readonly" );
+        const store = this.getObjectStore( storeName, 'readonly' );
         const data = [];
         store.openCursor( lb ).onsuccess = function ( osevt ) {
             const cursor = osevt.target.result;
