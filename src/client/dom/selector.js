a987f1cb8224d574fbddd31764a26964c3b48d2d
Joe Acosta <joe.acosta@sephora.com>
2023-02-16 15:06:37 -0800

    update selector code

--- src/client/dom/selector.js
+++ src/client/dom/selector.js
@@ -2,27 +2,23 @@
 // simple module to select elements based on css style selectors
 // internally calls documenet.getElementById or querySelectorAll based on regex
 // this is done in a UMD format
-import * as typeCheck from 'utils/typeCheck';
+import {
+    isString,
+    isObject
+} from 'utils/typeCheck';
 
 const rquickExpr = /^#(?:([\w-]+)|(\w+)|\.([\w-]+))$/;
 
-export default function selector( expr, parent ) {
-
-    let result, self = {},
-        qEle = expr;
-
-    self.length = 0;
-    self.objectName = 'selector';
-
-    const pObj = parent || document;
+export default function selector( expr, parent = document ) {
 
     // if it is not a string and it is an object
-    if ( !typeCheck.isString( qEle ) && typeCheck.isObject( qEle ) ) {
-        if ( qEle.hasOwnProperty( 'objectName' ) && qEle.objectName === 'selector' ) {
-            // can't select a selector object
-            return qEle;
-        }
+    if ( !isString( expr ) && isObject( expr ) &&
+        expr.hasOwnProperty( 'objectName' ) && expr.objectName === 'selector' ) {
+        // can't select a selector object
+        return expr;
     }
+
+    let qEle = expr;
 
     // html element so lets figure out what it is
     if ( expr instanceof HTMLElement ) {
@@ -36,31 +32,35 @@ export default function selector( expr, parent ) {
         }
     }
 
+    let result;
     if ( rquickExpr.test( qEle ) ) {
         // remove the leading # and return array of 1 or 0
-        result = pObj.querySelector( qEle );
+        result = parent.querySelector( qEle );
         result = ( result ? [ result ] : [] );
     } else {
-        result = pObj.querySelectorAll( qEle );
+        result = parent.querySelectorAll( qEle );
         result = ( result && result.length > 0 ? result : [] );
     }
 
-    self.length = result.length;
+    const selection = {
+        length: result.length,
+        objectName: 'selector'
+    };
 
-    self.get = function ( i ) {
+    selection.get = function ( i ) {
         return result[ i ];
     };
 
-    self.toArray = function () {
+    selection.toArray = function () {
         return Array.from( result );
     };
 
-    self.each = function ( fn ) {
-        self.toArray().forEach( fn );
+    selection.each = function ( fn ) {
+        selection.toArray().forEach( fn );
     };
 
-    for ( let y = 0; y < self.length; y++ ) {
-        self[ y ] = result[ y ];
+    for ( let y = 0; y < selection.length; y++ ) {
+        selection[ y ] = result[ y ];
     }
-    return self;
+    return selection;
 }
