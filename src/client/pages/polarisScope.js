d16747db8e7594ed8265a37157065a7d31c4eb38
Joe Acosta <joe.acosta@sephora.com>
2023-05-02 12:03:41 -0700

    use astronomia

--- src/client/pages/polarisScope.js
+++ src/client/pages/polarisScope.js
@@ -5,6 +5,13 @@ import {
 import WebWindow from 'client/components/wbWindow';
 import * as canvas from 'client/components/canvas';
 
+import {
+    julian,
+    sexa,
+    sidereal,
+    coord
+} from 'astronomia';
+
 function polarScope( x, y, size, anchors = [ '12/6', '0/0', '18/9', '6/3' ] ) {
 
     window.canvasRef.circle( x, y, size );
@@ -34,63 +41,26 @@ function polarScope( x, y, size, anchors = [ '12/6', '0/0', '18/9', '6/3' ] ) {
     window.canvasRef.addtext( x + size + 10, y, anchors[ 3 ] );
 }
 
-// get local sidereal time
-function calculateLST( longitude, now ) {
-
-    const julianDate = ( now.getTime() / 86400000 ) + 2440587.5;
-
-    const T = ( julianDate - 2451545.0 ) / 36525;
-    const LST = ( 100.46 + 0.985647 * julianDate + longitude + ( 15 * T ) ) % 360;
-    return Number( LST ).toFixed( 6 );
-}
-
-/*function hourAngleToDegrees(hour, minute, seconds) {
-    return ( +hour + ( minute / 60 ) + ( seconds / 3600 ) );
-}*/
-
-// this function calculated the right assention of polaris
-// we return the value to 4 decimal places
-// this is generally good enough for polar alignent 
-function getPolarisRightAssention( latitude, longitude, now ) {
-    const declination = 89.3619;
-
-    const julianDate = ( now.getTime() / 86400000 ) + 2440587.5;
-    const d = julianDate - 2451545;
-
-    const h = now.getUTCHours();
-    const m = now.getUTCMinutes();
-    const s = now.getUTCSeconds();
-
-    const GMST = 6.697375 + 0.0657098242 * d + h + ( m / 60 ) + ( s / 3600 );
-    const LST = GMST + ( longitude / 15 );
-
-    const polarisHourAngle = 2.53030128; // Hour Angle of Polaris at Greenwich on January 1, 2000, 12:00 UT
-
-    const HA = 15 * ( LST - polarisHourAngle );
-    const RA = Math.atan2( Math.sin( HA ), ( Math.cos( HA ) * Math.sin( latitude ) ) - ( Math.tan( declination ) * Math.cos( latitude ) ) );
-
-    return Number( RA ).toFixed( 6 );
-}
-
 function getPolarisHourAngle( latitude, longitude ) {
 
     // get utc time
     const now = new Date();
+    const year = now.getFullYear();
+    const month = `${now.getMonth() + 1}`.padStart( 2, '0' );
+    const dayOfMonth = `${now.getDate() + 1}`.padStart( 2, '0' );
 
-    const localSideRealTime = calculateLST( longitude, now );
+    const date = new Date( `${year}-${month}-${dayOfMonth}T00:00:00Z` );
+    const jd = new julian.CalendarGregorian().fromDate( date ).toJD();
 
-    const polarisRightAssention = getPolarisRightAssention( latitude, longitude, now );
+    const gst = sidereal.apparentGreenwich( jd );
+    const lst = ( gst + longitude / 360 ) * 24;
+    const lstHour = new sexa.HourAngle( lst );
 
-    let hourAnglePolaris = ( localSideRealTime - polarisRightAssention + 360 ) % 360;
-    if ( hourAnglePolaris > 180 ) {
-        hourAnglePolaris = hourAnglePolaris - 360;
-    }
-
-    return Number( hourAnglePolaris ).toFixed( 6 );
-}
-
-function getHourAngleClockTime( hourAnglePolaris ) {
-    return 24 - Number( hourAnglePolaris / 15 ).toFixed( 4 );
+    const polarisEcliptic = new coord.Ecliptic( latitude, longitude );
+    const polarisEquatorial = coord.equatorial.fromEcliptic( jd, polarisEcliptic );
+    const polarisRAHour = new sexa.HourAngle( polarisEquatorial.ra ).toString();
+    const polarisHA = new sexa.HourAngle( lstHour.sub( polarisRAHour ).hourAngle );
+    return polarisHA;
 }
 
 function setupPolarisHour() {
@@ -115,7 +85,7 @@ function setupPolarisHour() {
     const longitude = -122.47267;
     // lat long in degrees
     const polarisHourAngle = getPolarisHourAngle( latitude, longitude );
-    const clockTime = getHourAngleClockTime( polarisHourAngle );
+    const clockTime = '12'; //getHourAngleClockTime( polarisHourAngle );
 
     window.canvasRef.addtext( 50, 410, `Using latitude: ${latitude} and longitude: ${longitude}` );
     window.canvasRef.addtext( 50, 430, `Polaris hour angle: ${polarisHourAngle} and clock time: ${clockTime}` );
