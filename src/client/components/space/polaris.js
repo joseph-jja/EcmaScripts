0e41bb564e9f3ae38720679c734c86b4dfc365ad
Joe Acosta <joe.acosta@sephora.com>
2023-05-05 12:12:52 -0700

    finish migration of Date class to math utils

--- src/client/components/space/polaris.js
+++ src/client/components/space/polaris.js
@@ -125,12 +125,12 @@ class PolarScopeDateUtilities {
     // alternate method to calculate LST, which seems to do it right
     calculateLST( now, longitude ) {
 
-        const julianDate = ( now.getTime() / 86400000 ) + 2440587.5;
-        const D = julianDate - 2451545.0; // calculate number of days since January 1, 2000 at 12:00 UT
-        const UT = now.getUTCHours() + now.getUTCMinutes() / 60 + now.getUTCSeconds() / 3600; // calculate Universal Time
-        const GMST = 6.697374558 + 0.06570982441908 * D + 1.00273790935 * UT; // calculate Greenwich Mean Sidereal Time
-        const LST = PolarScopeUtilitiesInstance.getFraction( ( GMST + longitude / 15 ) / 24 ); // calculate local sidereal time
-        return ( 24 * LST ); // adjust for negative values
+        const julianDate = add( divide( now.getTime(), 86400000 ), 2440587.5 );
+        const D = subtract( julianDate, 2451545.0 ); // calculate number of days since January 1, 2000 at 12:00 UT
+        const UT = add( now.getUTCHours(), divide( now.getUTCMinutes(), 60 ), divide( now.getUTCSeconds(), 3600 ) ); // calculate Universal Time
+        const GMST = add( 6.697374558, multiply( 0.06570982441908, D ), multiply( 1.00273790935, UT ) ); // calculate Greenwich Mean Sidereal Time
+        const LST = PolarScopeUtilitiesInstance.getFraction( divide( add( GMST, divide( longitude, 15 ) ), 24 ) ); // calculate local sidereal time
+        return multiply( 24, LST ); // adjust for negative values
     }
 }
 
