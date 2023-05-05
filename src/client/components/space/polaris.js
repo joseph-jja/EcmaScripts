41d487e0430ddff8385b9509e92a4c048ad12e13
Joe <josepha48@gmail.com>
2023-05-04 21:28:08 -0700

    base ibject star

--- src/client/components/space/polaris.js
+++ src/client/components/space/polaris.js
@@ -224,8 +224,42 @@ class PolarisMath {
 
 const PolarisCalculator = new PolarisMath();
 
+import {
+    getCirclePoints
+} from 'utils/mathFunctions';
+
+import {
+    Star
+} from 'client/components/space/celestialBody';
+
+class PolarScope extends Star {
+    
+    constructor( options = {} ) {
+        super( options );
+        
+        this.latitude = options.latitude;
+        this.longitude = options.longitude;
+        this.rightAssention = options.rightAssention;
+    }
+
+    setupPoints( xRadius = 30 ) {
+        this.points = getCirclePoints( xRadius );
+    }
+    
+    increment() {
+        
+        this.angle = this.direction( this.angle, this.speed );
+        if ( this.angle < 0 ) {
+            this.angle = 360;
+        } else if ( this.angle > 360 ) {
+            this.angle = 0;
+        }
+    }
+}
+
 export {
     utils,
     dateUtils,
-    PolarisCalculator
+    PolarisCalculator,
+    PolarScope
 };
