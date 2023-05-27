752af43896132a2fad1729da1e74d4c35b373fdd
Joe <josepha48@gmail.com>
2023-05-27 13:25:52 -0700

    fix histogram

--- src/client/pages/histogramCanvas.js
+++ src/client/pages/histogramCanvas.js
@@ -8,6 +8,8 @@ function histogramCanvas() {
 
     window.histogramCanvasRef = canvas.create( "histogram-display", "center-aligner", 520, 450 );
 
+    document.getElementById('center-aligner').prepend(document.getElementById("histogram-display"));
+
 }
 
 addOnLoad( histogramCanvas );
