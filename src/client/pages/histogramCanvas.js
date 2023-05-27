879b3b4cee98742aa276262e82ba924f455583ba
Joe <josepha48@gmail.com>
2023-05-27 13:08:52 -0700

    change name of exported object

--- src/client/pages/histogramCanvas.js
+++ src/client/pages/histogramCanvas.js
@@ -6,7 +6,7 @@ import * as canvas from 'client/components/canvas';
 
 function histogramCanvas() {
 
-    window.canvasRef = canvas.create( "center-aligner", "histogram-display", 520, 450 );
+    window.histogramCanvasRef = canvas.create( "center-aligner", "histogram-display", 520, 450 );
 
 }
 
