2166adb03460f8aaa018bb42bbb5547ed534399e
Joe Acosta <joe.acosta@sephora.com>
2023-05-28 14:06:48 -0700

    upate id

--- src/client/pages/histogramCanvas.js
+++ src/client/pages/histogramCanvas.js
@@ -2,9 +2,9 @@ import * as canvas from 'client/components/canvas';
 
 window.addEventListener('DOMContentLoaded', () => {
 
-    window.histogramCanvasRef = canvas.create( "histogram-display", "image-container", 520, 450 );
+    window.histogramCanvasRef = canvas.create( 'histogram-display', 'image-container', 520, 450 );
 
-    document.getElementById('center-aligner').prepend(document.getElementById("histogram-display"));
+    document.getElementById('image-container').prepend(document.getElementById('histogram-display'));
 
 });
 
