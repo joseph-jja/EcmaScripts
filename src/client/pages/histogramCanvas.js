281ace314da0aa9dabe907573630e0a486ce9f81
Joe Acosta <joe.acosta@sephora.com>
2023-05-28 14:02:50 -0700

    upate id

--- src/client/pages/histogramCanvas.js
+++ src/client/pages/histogramCanvas.js
@@ -2,7 +2,7 @@ import * as canvas from 'client/components/canvas';
 
 window.addEventListener('DOMContentLoaded', () => {
 
-    window.histogramCanvasRef = canvas.create( "histogram-display", "center-aligner", 520, 450 );
+    window.histogramCanvasRef = canvas.create( "histogram-display", "image-container", 520, 450 );
 
     document.getElementById('center-aligner').prepend(document.getElementById("histogram-display"));
 
