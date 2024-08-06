297dc3464352c8ef722ec1d37dd9453bf98583c8
Joe Acosta <joe.acosta@sephora.com>
2024-08-06 14:23:13 -0700

    update some code to make it cleaner

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -1,113 +1,105 @@
 const decode = decodeURIComponent;
 const encode = encodeURIComponent;
 
-const COOKIE_MATCH = /([^=]+)=/i,
-    SPLIT_COOKIES = /; /g;
+const SPLIT_COOKIES = /; /g;
 
-const decodeKeyValue = ( cookie ) => {
-    const ck = cookie.match( COOKIE_MATCH );
+const decodeKeyValue = (cookieData) => {
+    const idx = cookieData.indexOf('=');
     const result = {};
-    if ( ck instanceof Array ) {
-        try {
-            result[ 'name' ] = decode( ck[ 1 ] );
-            result[ 'value' ] = decode( cookie.substring( ck[ 1 ].length + 1 ) );
-        } catch ( ex ) {
-            // ignore
-        }
-    } else {
-        result[ 'name' ] = decode( cookie );
+    if (idx > -1) {
+        result['name'] = decode(cookieData.substring(0, idx));
+        result['value'] = decode(cookieData.substring(idx + 1));
     }
     return result;
 };
 
-function findCookieByName( cookieName, cookieData ) {
+const findCookieByName = (cookieName, cookieData) => {
 
-    // this will throw if in server mode and there is no document object :) 
-    const dc = ( cookieData || document.cookie || '' ),
-        cookies = dc.split( SPLIT_COOKIES );
+    const dc = (cookieData || (typeof document !== 'undefined' && document.cookie) || ''),
+        cookies = dc.split(SPLIT_COOKIES);
 
-    const result = cookies.map( cookie => {
+    const result = cookies.map(cookie => {
         const {
             name,
             value
-        } = decodeKeyValue( cookie );
+        } = decodeKeyValue(cookie);
         return {
             name,
             value
         };
-    } ).filter( cookie => {
+    }).filter(cookie => {
         const {
             name
         } = cookie;
-        return ( name === cookieName );
-    } ).map( cookie => {
+        return (name === cookieName);
+    }).map(cookie => {
         const {
             value
         } = cookie;
         return value;
-    } );
-    return result[ 0 ];
+    });
+    return result[0];
 };
 
 // can be used both server side or client side
-function get( name, cookieData ) {
-    return findCookieByName( name, cookieData );
+function get(name, cookieData) {
+    return findCookieByName(name, cookieData);
 };
 
-function checkOption( options, opt, useVal ) {
+function checkOption(options, opt, useVal) {
 
     let result = '';
 
-    if ( typeof options !== 'undefined' && options[ opt ] ) {
-        result = ";" + opt + ( useVal ? "=" + options[ opt ] : '' );
+    if (typeof options !== 'undefined' && options[opt]) {
+        result = ";" + opt + (useVal ? "=" + options[opt] : '');
     }
     return result;
 }
 
-function set( name, value, options ) {
+function set(name, value, options) {
     let ename, evalue, data,
         isServer;
 
-    ename = encode( name );
-    evalue = encode( value );
+    ename = encode(name);
+    evalue = encode(value);
 
     data = ename + "=" + evalue;
 
-    data += checkOption( options, 'path', true );
-    data += checkOption( options, 'domain', true );
+    data += checkOption(options, 'path', true);
+    data += checkOption(options, 'domain', true);
 
-    isServer = ( ( checkOption( options, 'server' ) !== '' ) ? true : false );
+    isServer = ((checkOption(options, 'server') !== '') ? true : false);
 
-    data += checkOption( options, 'expires', true );
-    data += checkOption( options, 'Max-Age', true );
+    data += checkOption(options, 'expires', true);
+    data += checkOption(options, 'Max-Age', true);
 
-    data += checkOption( options, 'Secure', false );
-    data += checkOption( options, 'HttpOnly', false );
-    data += checkOption( options, 'SameSite', true );
+    data += checkOption(options, 'Secure', false);
+    data += checkOption(options, 'HttpOnly', false);
+    data += checkOption(options, 'SameSite', true);
 
-    if ( !isServer ) {
+    if (!isServer) {
         document.cookie = data;
     }
     return data;
 };
 
-function remove( name ) {
+function remove(name) {
     const now = new Date(),
-        exists = findCookieByName( name );
-    if ( exists ) {
-        now.setFullYear( 1970 );
-        set( name, undefined, {
+        exists = findCookieByName(name);
+    if (exists) {
+        now.setFullYear(1970);
+        set(name, undefined, {
             expires: now
-        } );
+        });
     }
 };
 
-function count( cookieData ) {
-    return ( cookieData || document.cookie ).split( SPLIT_COOKIES ).length;
+function count(cookieData) {
+    return (cookieData || document.cookie).split(SPLIT_COOKIES).length;
 };
 
-function length( cookieData ) {
-    return ( cookieData || document.cookie ).length;
+function length(cookieData) {
+    return (cookieData || document.cookie).length;
 };
 
 export {
