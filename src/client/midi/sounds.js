6eef672adcca8e4238ad7c332ee69a63ebaf0ab4
josepha48 <josepha48@gmail.com>
2025-04-18 20:03:30 -0700

    add sound file to create basic sounds

---
+++ src/client/midi/sounds.js
@@ -0,0 +1,41 @@
+const WAVEFORM_TYPES = [
+    'sine',
+    'triangle',
+    'square',
+    'sawtooth'
+];
+
+class MakeSound {
+
+    constructor() {
+        this.audioContext = new AudioContext();
+    }
+
+    // Set the type of wave (sine, triangle, square, sawtooth)
+    getOscillator(waveform) {
+        const oscillator = this.audioContext.createOscillator();
+        oscillator.type = WAVEFORM_TYPES.includes(waveform) ? waveform : 'sawtooth';
+        return oscillator;
+    }
+
+    setNote(oscillator, note) {
+        // Set the frequency of the oscillator (in Hz)
+        oscillator.frequency.value = 60; // A3 note
+        
+        // Connect the oscillator to the audio context destination (speakers)
+        oscillator.connect(audioContext.destination);
+    }
+
+    playNote(oscillator, duration) {
+        // Start the oscillator
+        oscillator.start();
+        setTimeout(() => {
+            oscillator.stop();
+        }, duration);
+    }
+}
+
+export {
+    WAVEFORM_TYPES,
+    MakeSound
+};
