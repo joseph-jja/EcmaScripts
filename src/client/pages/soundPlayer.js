d9480a85a87471fa36ec2a7b75825dd8a429caf8
Joe A <josepha48.nospam@gmail.com>
2025-04-21 16:47:22 -0700

    add in some basis stuff

--- src/client/pages/soundPlayer.js
+++ src/client/pages/soundPlayer.js
@@ -1,3 +1,33 @@
+import "@babel/runtime/regenerator";
+
+import selector from '/js/client/dom/selector';
+import * as events from '/js/client/dom/events';
+import * as dom from '/jsclient/dom/DOM';
+
 import {
 	MakeSound
 } from '/js/client/midi/sounds.js';
+
+// window
+import WebWindow from 'client/components/wbWindow';
+
+function getMainWindow() {
+    const mw = document.getElementById( 'main-window' );
+    const styles = window.getComputedStyle( mw );
+
+    return new WebWindow( 'BART Info',
+        styles.offsetLeft,
+        styles.offsetTop,
+        styles.offsetWidth,
+        styles.offsetHeight,
+        'main-window' );
+}
+
+async function doOnLoadStuff() {
+
+
+
+}
+
+events.addOnLoad( doOnLoadStuff );
+
