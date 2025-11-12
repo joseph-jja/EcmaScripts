5289f81f1f11b9d737b0c176b824524a5382366a
Joe <josepha48@gmail.com>
2025-11-12 10:41:02 -0800

    Refactor date utilities to AstronomyDateUtilities

--- src/client/components/space/PolarScopeCalculator.js
+++ src/client/components/space/PolarScopeCalculator.js
@@ -21,86 +21,13 @@ const SigmaOctantis = {
     Declination: -77.4311111
 };
 
-class PolarScopeDateUtilities {
-
-    // takes new Date() object
-    toUTC( d ) {
-        return {
-            year: d.getUTCFullYear(),
-            month: d.getUTCMonth(),
-            date: d.getUTCDate(),
-            hours: d.getUTCHours(),
-            minutes: d.getUTCMinutes(),
-            seconds: d.getUTCSeconds()
-        };
-    }
-
-    // takes output of toUTC called internal
-    toJulien( d ) {
-        //    const julianDate = ( now.getTime() / 86400000 ) + 2440587.5;
-        const t = d.date;
-        let n = add( d.month, 1 ),
-            r = d.year,
-            i = add( d.hours, divide( d.minutes, 60 ), divide( d.seconds, 3600 ) ); // universal time
-        if ( n <= 2 ) {
-            n = add( n, 12 );
-            r = subtract( r, 1 );
-        }
-        const p = Math.floor( divide( r, 100 ) );
-        const q = Math.floor( multiply( 365.25, add( r, 4716 ) ) );
-        const u = Math.floor( multiply( 30.6001, add( n, 1 ) ) );
-        const s = add( subtract( subtract( add( q, u, t ), 13 ), 1524.5 ), divide( i, 24 ) );
-        return ( n <= 2 && ( n, r ), p, s );
-    }
-
-    // takes output of toJulien called internal
-    toGMST( d ) {
-        const r = subtract( d, 2400000.5 );
-        const n = Math.floor( r );
-        const t = divide( subtract( n, 51544.5 ), 36525 );
-        const u = multiply( subtract( .093104, multiply( 62e-7, t ) ), t );
-        const v = multiply( add( 8640184.812866, u ), divide( t, 3600 ) );
-        return add( 6.697374558, multiply( 24, subtract( r, n ), 1.0027379093 ), v );
-    }
-
-    // takes output of toGMST and longitude called internal
-    gmstToLST( d, longitude ) {
-        return multiply( 24, DateUtilities.getFraction( divide( add( d, divide( longitude, 15 ) ), 24 ) ) );
-    }
-
-    isDST( now ) {
-        const jan = new Date( now.getFullYear(), 0, 1 ).getTimezoneOffset();
-        const jul = new Date( now.getFullYear(), 6, 1 ).getTimezoneOffset();
-        return Math.max( jan, jul ) !== now.getTimezoneOffset();
-    }
-
-    // calcualte LST
-    utcToLST( d, longitude ) {
-        const n = this.toJulien( d ),
-            r = this.toGMST( n );
-        return this.gmstToLST( r, longitude );
-    }
-
-    // alternate method to calculate LST, which seems to do it right
-    calculateLST( now, longitude ) {
-
-        const julianDate = add( divide( now.getTime(), 86400000 ), 2440587.5 );
-        const D = subtract( julianDate, 2451545.0 ); // calculate number of days since January 1, 2000 at 12:00 UT
-        const UT = add( now.getUTCHours(), divide( now.getUTCMinutes(), 60 ), divide( now.getUTCSeconds(), 3600 ) ); // calculate Universal Time
-        const GMST = add( 6.697374558, multiply( 0.06570982441908, D ), multiply( 1.00273790935, UT ) ); // calculate Greenwich Mean Sidereal Time
-        const LST = DateUtilities.getFraction( divide( add( GMST, divide( longitude, 15 ) ), 24 ) ); // calculate local sidereal time
-        return multiply( 24, LST ); // adjust for negative values
-    }
-}
-
-const PolarScopeDateUtilitiesInstance = new PolarScopeDateUtilities();
 
 class PolarScopeCalculator {
 
     // calculate offset of Polaris 
     // OMG crazy maths
     precessionCorrection( e, latitude ) {
-        let t = PolarScopeDateUtilitiesInstance.toJulien( e ),
+        let t = AstronomyDateUtilitiesInstance.toJulien( e ),
             n = Polaris.RightAscension,
             r = Polaris.Declination;
 
@@ -145,15 +72,15 @@ class PolarScopeCalculator {
                 this.correctedDEC = subtract( -180, this.correctedDEC );
                 this.correctedRA = add( this.correctedRA, 12 );
             }
-            this.correctedRA = DateUtilities.mapTo24Hour( this.correctedRA );
+            this.correctedRA = AstronomyDateUtilitiesInstance.mapTo24Hour( this.correctedRA );
         }
     }
 
     // from the dot com
     getPolarisHA( now, latitude, longitude ) {
-        const utcNow = PolarScopeDateUtilitiesInstance.toUTC( now );
+        const utcNow = AstronomyDateUtilitiesInstance.toUTC( now );
         this.precessionCorrection( utcNow, latitude );
-        const lst = PolarScopeDateUtilitiesInstance.utcToLST( utcNow, longitude );
+        const lst = AstronomyDateUtilitiesInstance.utcToLST( utcNow, longitude );
         let t = subtract( lst, this.correctedRA );
         if ( latitude < 0 ) {
             if ( t < 0 ) {
@@ -174,10 +101,10 @@ class PolarScopeCalculator {
     getPolarisHourAngle( now, latitude, longitude, rightAssention ) {
 
         // get utc time
-        const utcNow = PolarScopeDateUtilitiesInstance.toUTC( now );
+        const utcNow = AstronomyDateUtilitiesInstance.toUTC( now );
         this.precessionCorrection( utcNow, latitude );
 
-        const localSideRealTime = PolarScopeDateUtilitiesInstance.calculateLST( now, longitude );
+        const localSideRealTime = AstronomyDateUtilitiesInstance.calculateLST( now, longitude );
 
         let hourAnglePolaris = Number( subtract( localSideRealTime, rightAssention ) ).toFixed( 6 );
         let plusHourAnglePolaris = Number( subtract( localSideRealTime, this.correctedRA ) ).toFixed( 6 );
@@ -202,9 +129,4 @@ class PolarScopeCalculator {
 }
 
 const PolarisCalculatorInstance = new PolarScopeCalculator();
-
-export {
-    DateUtilities,
-    PolarScopeDateUtilitiesInstance,
-    PolarisCalculatorInstance
-};
+export default PolarisCalculatorInstance;
