cb19bfd96d42536772d5825f74e68568b34c3be8
Joe <josepha48@gmail.com>
2025-11-12 10:26:16 -0800

    Refactor gmstToLST to use DateUtilities methods

--- src/client/components/space/DateUtilities.js
+++ src/client/components/space/DateUtilities.js
@@ -49,7 +49,7 @@ class DateUtilities {
 
     // takes output of toGMST and longitude called internal
     gmstToLST( d, longitude ) {
-        return multiply( 24, PolarScopeUtilitiesInstance.getFraction( divide( add( d, divide( longitude, 15 ) ), 24 ) ) );
+        return multiply( 24, DateUtilities.getFraction( divide( add( d, divide( longitude, 15 ) ), 24 ) ) );
     }
 
     isDST( now ) {
@@ -72,7 +72,7 @@ class DateUtilities {
         const D = subtract( julianDate, 2451545.0 ); // calculate number of days since January 1, 2000 at 12:00 UT
         const UT = add( now.getUTCHours(), divide( now.getUTCMinutes(), 60 ), divide( now.getUTCSeconds(), 3600 ) ); // calculate Universal Time
         const GMST = add( 6.697374558, multiply( 0.06570982441908, D ), multiply( 1.00273790935, UT ) ); // calculate Greenwich Mean Sidereal Time
-        const LST = PolarScopeUtilitiesInstance.getFraction( divide( add( GMST, divide( longitude, 15 ) ), 24 ) ); // calculate local sidereal time
+        const LST = DateUtilities.getFraction( divide( add( GMST, divide( longitude, 15 ) ), 24 ) ); // calculate local sidereal time
         return multiply( 24, LST ); // adjust for negative values
     }
 }
