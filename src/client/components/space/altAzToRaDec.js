6d235231356fca53d8aa2a8b3c869fa182df4407
Joe <josepha48@gmail.com>
2025-11-12 13:50:54 -0800

    Refactor altAzToRaDec function calculations

--- src/client/components/space/altAzToRaDec.js
+++ src/client/components/space/altAzToRaDec.js
@@ -7,9 +7,14 @@ import {
 import AstronomyMathUtilitiesInstance from '/js//client/components/space/AstronomyMathUtilities';
 import AstronomyDateUtilitiesInstance from '/js//client/components/space/AstronomyDateUtilities';
 
+// alt, az, lat, long are in degrees
 export default altAzToRaDec(alt, az, lat, lon, localTime) {
 
-  const lst = AstronomyDateUtilitiesInstance.calculateLST(localTime);
+    const lst = AstronomyDateUtilitiesInstance.calculateLST(localTime);
+
+    const sinDec = add(multiply(Math.sin(alt), Math.sin(lat)), multiply(Math.cos(alt), Math.cos(lat), Math.cos(az)));
+    const hourAngle = divide(subtract(Math.sin(alt), multiply(Math.sin(lat), sinDec)), multiply(Math.cos(lat), Math.cos(az)));
+
   
 
 };
