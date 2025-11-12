7b451f4d1977553d5153ecd1bab00de7cd6fbc4c
Joe <josepha48@gmail.com>
2025-11-12 10:38:10 -0800

    Refactor PolarScopeCalculator and update imports
    Refactored PolarScopeCalculator by removing PolarScopeUtilities class and importing AstronomyMathUtilities and AstronomyDateUtilities.

--- src/client/components/space/PolarScopeCalculator.js
+++ src/client/components/space/PolarScopeCalculator.js
@@ -4,7 +4,8 @@ import {
     multiply,
     divide
 } from '/js/utils/mathFunctions';
-import DateUtilities from '/js//client/components/space/DateUtilities';
+import AstronomyMathUtilitiesInstance from '/js//client/components/space/AstronomyMathUtilitiess';
+import AstronomyDateUtilitiesInstance from '/js//client/components/space/AstronomyDateUtilities';
 
 // some of this code was taken from takahashi-europe.com minified code
 // unminfied and redone to make some sense 
@@ -19,48 +20,6 @@ const SigmaOctantis = {
     RightAscension: 21.0786056,
     Declination: -77.4311111
 };
-
-class PolarScopeUtilities {
-
-    // degrees to hour angle
-    hoursMinutesSeconds( degrees ) {
-        let hours = Math.floor( degrees );
-        hours = ( hours < 0 ? add( 24, hours ) : hours );
-        hours = ( hours > 24 ? hours % 24 : hours );
-
-        const minutes = this.pad( Math.floor( multiply( 60, this.getFraction( degrees ) ) ) );
-        const seconds = this.pad( Math.round( multiply( 60, subtract( multiply( 60, this.getFraction( degrees ) ), minutes ) ) ) );
-
-        return `${ hours }:${ minutes }:${ seconds }`;
-    }
-
-    // takes hour angle and converts to degrees
-    hourAngleToDegrees( hour, minute, seconds ) {
-        return add( hour, divide( minute, 60 ), divide( seconds, 3600 ) );
-    }
-
-    // 24 hour clock :?
-    mapTo24Hour( hour ) {
-        let result = hour;
-        if ( result < 0 ) {
-            result = subtract( result, multiply( 24, subtract( divide( result, 24 ), 1 ) ) );
-        } else if ( hour >= 24 ) {
-            result = subtract( result, multiply( divide( result, 24 ), 24 ) );
-        }
-        return result;
-    }
-
-    pad( x ) {
-        return `${x}`.padStart( 2, '0' );
-    }
-
-    getFraction( num ) {
-        // get decimal places
-        const decimals = subtract( num, Math.floor( num ) );
-        // we want positive number
-        return ( decimals < 0 ? add( decimals, 1 ) : decimals );
-    }
-}
 
 class PolarScopeDateUtilities {
 
