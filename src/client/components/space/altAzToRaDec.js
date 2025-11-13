9ade414fd20d77b132327acc53fb8acaefa03b67
josepha48 <josepha48@gmail.com>
2025-11-12 21:51:17 -0800

    add another method for julien date

--- src/client/components/space/altAzToRaDec.js
+++ src/client/components/space/altAzToRaDec.js
@@ -10,7 +10,8 @@ import AstronomyDateUtilitiesInstance from '/js//client/components/space/Astrono
 // alt, az, lat, long are in degrees
 export default function altAzToRaDec(alt, az, lat, lon, localTime) {
 
-    const lst = AstronomyDateUtilitiesInstance.calculateLST(localTime);
+    const lst = AstronomyDateUtilitiesInstance.calculateLST(localTime, lon);
+    const lstt = AstronomyDateUtilitiesInstance.utcToL(AstronomyDateUtilitiesInstance.toUTC(localTime), lon);
 
     const sinDec = add(multiply(Math.sin(alt), Math.sin(lat)), multiply(Math.cos(alt), Math.cos(lat), Math.cos(az)));
     const hourAngleRad = divide(subtract(Math.sin(alt), multiply(Math.sin(lat), sinDec)), multiply(Math.cos(lat), Math.cos(az)));
