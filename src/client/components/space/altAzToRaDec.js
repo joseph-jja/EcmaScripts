b44a75f1f69e3c4d3de7bcbc89de8d7aa043487f
Joe <josepha48@gmail.com>
2025-11-13 09:16:42 -0800

    Fix function calls and variable names in altAzToRaDec

--- src/client/components/space/altAzToRaDec.js
+++ src/client/components/space/altAzToRaDec.js
@@ -11,15 +11,15 @@ import AstronomyDateUtilitiesInstance from '/js//client/components/space/Astrono
 export default function altAzToRaDec(alt, az, lat, lon, localTime) {
 
     const lst = AstronomyDateUtilitiesInstance.calculateLST(localTime, lon);
-    const lstt = AstronomyDateUtilitiesInstance.utcToL(AstronomyDateUtilitiesInstance.toUTC(localTime), lon);
+    const lstt = AstronomyDateUtilitiesInstance.utcToLST(AstronomyDateUtilitiesInstance.toUTC(localTime), lon);
 
     const sinDec = add(multiply(Math.sin(alt), Math.sin(lat)), multiply(Math.cos(alt), Math.cos(lat), Math.cos(az)));
     const hourAngleRad = divide(subtract(Math.sin(alt), multiply(Math.sin(lat), sinDec)), multiply(Math.cos(lat), Math.cos(az)));
 
-    const dec = math.asin(sinDec);
+    const dec = Math.asin(sinDec);
     const hourAngle = Math.asin(hourAngleRad);
 
-    const ra = subtgract(lst, hourAngle);
+    const ra = subtract(lst, hourAngle);
   
     return {
         dec,
