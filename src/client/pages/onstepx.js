09cc1e4563d9eca954c236fe5c519276c296a646
Joe <josepha48@gmail.com>
2025-11-19 10:19:16 -0800

    Remove mathFunctions

--- src/client/pages/onstepx.js
+++ src/client/pages/onstepx.js
@@ -1,5 +1,4 @@
 // these are to export to play with 
-import mathFunctions from '/js/utils/mathFunctions';
 import AstronomyMathUtilitiesInstance from '/js/client/components/space/AstronomyMathUtilities';
 import AstronomyDateUtilitiesInstance from '/js/client/components/space/AstronomyDateUtilities';
 import altAzToRaDec from '/js/client/components/space/altAzToRaDec';
@@ -7,4 +6,3 @@ import altAzToRaDec from '/js/client/components/space/altAzToRaDec';
 window.AMU = AstronomyMathUtilitiesInstance;
 window.ADU = AstronomyDateUtilitiesInstance;
 window.altAzToRaDec = altAzToRaDec;
-window.mathFunctions = mathFunctions;
