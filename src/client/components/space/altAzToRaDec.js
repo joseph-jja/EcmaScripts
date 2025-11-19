9a95cd8cc99b251382b4e09c66872e143f6a7c90
Joe <josepha48@gmail.com>
2025-11-19 09:55:20 -0800

    Clean up the code and only keep working code

--- src/client/components/space/altAzToRaDec.js
+++ src/client/components/space/altAzToRaDec.js
@@ -41,41 +41,20 @@ export default function altAzToRaDec( alt, az, lat, lon, localTime ) {
     // this is the dec in degrees xxx.yyyyy
     const dec = radiansToDegrees( decR );
 
-    // both these methods of computing hour angle come up with same value
+    // this method works correctly however the local time must be accurate
     const hourAngleX = divide( multiply( multiply( -1, sinAz ), cosAlt ), cosDecR );
     const hourAngleY = divide( subtract( sinAlt, multiply( sinDec, sinLat ) ), multiply( cosDecR, cosLat ) );
     const hourAngle = radiansToDegrees( Math.atan2( hourAngleX, hourAngleY ) );
-
-    // this method is occassionally off 
-    const hourAngleAlt = radiansToDegrees( divide( subtract( sinAlt, multiply( sinLat, sinDec ) ), multiply( cosLat, cosDecR ) ) );
-
-    // third try
-    const angleOne = multiply( cosAz, sinLat );
-    const angleTwo = multiply( -1, sinAz );
-    const angleThree = multiply( Math.tan( alt ), cosLat );
-    let hourAngleThree = Math.atan2( angleTwo, subtract( angleThree, angleOne ) );
-    if ( hourAngleThree < 0 ) {
-        hourAngleThree += Math.PI * 2;
-    }
-
     // now compute ra
     const ra = subtract( lst, hourAngle );
-    const raAlt = subtract( lst, hourAngleAlt );
-    const raThree = subtract( lst, hourAngleThree );
 
     const decInHMS = AstronomyMathUtilitiesInstance.decDegreesToHourMinutesSeconds( dec );
     const raInHMS = AstronomyMathUtilitiesInstance.raDegreesToHourMinutesSeconds( ra );
-    const raInHMSAlt = AstronomyMathUtilitiesInstance.raDegreesToHourMinutesSeconds( raAlt );
-    const raInHMSThree = AstronomyMathUtilitiesInstance.raDegreesToHourMinutesSeconds( raThree );
 
     return {
         dec,
         decInHMS,
         ra,
-        raInHMS,
-        raAlt,
-        raInHMSAlt,
-        raThree,
-        raInHMSThree
+        raInHMS
     };
 };
