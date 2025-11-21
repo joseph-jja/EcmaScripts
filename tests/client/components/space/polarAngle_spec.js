bc7311947c46d47c82682b38ccd534e2c0aee964
Joe <josepha48@gmail.com>
2025-11-21 11:49:16 -0800

    Add in a test for better polaris hour angle calculations

--- tests/client/components/space/polarAngle_spec.js
+++ tests/client/components/space/polarAngle_spec.js
@@ -1,41 +1,56 @@
-/*import altAzToRaDec from 'client/components/space/altAzToRaDec';
+import PolarisCalculatorInstance from '/js/client/components/space/PolarScopeCalculator';
 import AstronomyMathUtilitiesInstance from '/js/client/components/space/AstronomyMathUtilities';
-import AstronomyDateUtilitiesInstance from '/js/client/components/space/AstronomyDateUtilities';
 
-const ADU = AstronomyDateUtilitiesInstance;
 const AMU = AstronomyMathUtilitiesInstance;
 
-describe( 'testing polaris hour angle calculations', () => {
+fdescribe( 'testing polaris hour angle calculations', () => {
 
-    fit('get LST right', () => {
+    it('polar hour angle test 1', () => {
 
-        const latitude = 38.3862;
+        const latitude = 38.20;
         const longitude = -121.991;
 
         const now = new Date();
         now.setFullYear(2025);
         now.setMonth(10);
-        now.setDate(20);
-        now.setHours(21);
-        now.setMinutes(30);
-        now.setSeconds(43);
+        now.setDate(21);
+        now.setHours(10);
+        now.setMinutes(53);
+        now.setSeconds(19);
         now.setMilliseconds(0);
-        const localTime = new Date(now.getTime());
 
-        const utcTime = ADU.toUTC( localTime );
-        const julianDate = ADU.toJulian( utcTime );
-        const gmst = ADU.toGMST( julianDate );
-        const lst = ADU.gmstToLST( gmst, longitude );
+        const polarisRA = 2 + (31/60) + (49.09/3600); // 2.5303027777777777
+        const polarisRA2 = 3.1208056;
 
-        //expect(lst).toEqual('4.367991043177');//'353.459038204435');
-        const hourAngle = 14 + (7/60) + (50/360); 
-        const ha = ((lst - 3.1208056));// * 24) % 15;
-        //const ra = lst - hourAngle;
-        expect( ha ).toEqual(hourAngle);
-        //expect( ra ).toEqual(12.420156195565028);
-        //expect(AMU.hoursMinutesSecondsTo24(ha2)).toEqual('12:15');
-        //expect(AMU.hoursMinutesSecondsTo24(12.420156195565028)).toEqual('12:25:13');
+        const hourAngle = PolarisCalculatorInstance.calculateHourAngle(now, polarisRA, longitude);
+
+        expect( +Number(hourAngle).toFixed(0) ).toEqual(4);
+        expect(AMU.hoursMinutesSecondsTo24(hourAngle)).toEqual('4:17:38');
 
     });
+
+    /*it('polar hour angle test 2', () => {
+
+        const latitude = 38.20;
+        const longitude = -121.991;
+
+        const now = new Date();
+        now.setFullYear(2025);
+        now.setMonth(10);
+        now.setDate(25);
+        now.setHours(19);
+        now.setMinutes(38);
+        now.setSeconds(50);
+        now.setMilliseconds(0);
+
+        const polarisRA = 2 + (31/60) + (49.09/3600); // 2.5303027777777777
+        const polarisRA2 = 3.1208056;
+
+        const hourAngle = PolarisCalculatorInstance.calculateHourAngle(now, polarisRA, longitude);
+
+        expect( +Number(hourAngle).toFixed(0) ).toEqual(5);
+        expect(AMU.hoursMinutesSecondsTo24(hourAngle)).toEqual('5:03:17');
+
+    });*/
 });
-*/
+
